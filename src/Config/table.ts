43079863d92f53342ba9b7efe5fdb953c96fd400
Licethd <liceth.dfe@gmail.com>
2024-06-19 20:44:24 -0400

    mejoras

---
+++ src/Config/table.ts
@@ -0,0 +1,57 @@
+import { SThemeThemes } from 'servisofts-component';
+// #166ED8 AM
+// #F7282F RED
+// const theme: SThemeThemes = {
+//     default: {
+//         barStyle: 'dark-content',
+//         barColor: '#ffffff',
+//         text: '#000000',
+//         primary: '#ffffff',
+//         secondary: '#000000',
+//         info: '#97989d',
+//         background: '#ffffff',
+//         card: '#EBBCBD33',
+//         font: "OpenSans"
+//     },
+//     dark: {
+//         barStyle: 'light-content',
+//         barColor: '#000000',
+//         text: '#ffffff',
+//         primary: '#000000',
+//         secondary: '#ffffff',
+//         info: '#6E6F74',
+//         background: '#000000',
+//         card: '#F7282F22',
+//         font: "OpenSans"
+//     }
+// }
+
+const theme: SThemeThemes = {
+    default: {
+        barStyle: 'dark-content',
+        barColor: '#ffffff',
+        text: '#000000',
+        primary: '#ffffff',
+        secondary: '#000000',
+        info: '#97989d',
+        background: '#ffffff',
+        // card: '#00000010',
+        card: '#00000015',
+        font: "Roboto",
+        accent: "#B5B5B7"
+    },
+    dark: {
+        barStyle: 'light-content',
+        barColor: '#000000',
+        text: '#ffffff',
+        primary: '#000000',
+        secondary: '#ffffff',
+        info: '#6E6F74',
+        background: '#000000',
+        card: '#ffffff22',
+        font: "Roboto",
+        // card: '#ffffff10'
+        accent: "#3A3A3A"
+    }
+}
+export default theme;
