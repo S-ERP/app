3711104dbe5e0d90c86f80fe2d811e64c5ca9098
Licethd <liceth.dfe@gmail.com>
2025-05-29 00:16:50 -0400

    f5

--- src/Pages/crm/proyecto.js
+++ src/Pages/crm/proyecto.js
@@ -1,79 +1,158 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { SIcon, SNotification, SPage, SText, STheme, SView } from 'servisofts-component';
+import { SIcon, SNotification, SPage, SPopup, SText, STheme, SView } from 'servisofts-component';
 import FormRegistroProyecto from './Components/FormRegistroProyecto';
 import MDL from '../../MDL';
 import { DinamicTable } from 'servisofts-table';
 import SSocket from "servisofts-socket";
+import FloatButtom from '../../Components/FloatButtom';
+import RolesDelUsuario from '../rol/Components/RolesDelUsuario';
+import FormEditarUsuario from '../usuario/Components/FormEditarUsuario';
+import FormEditarPassword from '../usuario/Components/FormEditarPassword';
+import FloatMenu from '../../Components/FloatMenu';
 
 
 export default class proyecto extends Component {
 
- componentDidMount() {
-  MDL.crm.proyecto.getAll().then(e => {
-   console.log("Projects fetched successfully:", e);
-  }).catch(e => {
-   console.error("Error fetching projects:", e);
-  })
- }
+        componentDidMount() {
+                MDL.crm.proyecto.getAll().then(e => {
+                        console.log("Projects fetched successfully:", e);
+                }).catch(e => {
+                        console.error("Error fetching projects:", e);
+                })
+        }
 
- render() {
-  return <SPage title={"Proyecto"}>
+        render() {
+                return <SPage title={"Proyecto"} icon={<SIcon name='empresa' fill={STheme.color.text} />}>
 
-   <SText onPress={() => {
-    FormRegistroProyecto.open(({
-     onRegister: (e) => {
-      this.DinamicTable.loadData();
-     } })) }}>{"Agregar proyecto"}</SText>
+                        {/* <SText onPress={() => {
+                                FormRegistroProyecto.open(({
+                                        onRegister: (e) => {
+                                                this.DinamicTable.loadData();
+                                        }
+                                }))
+                        }}>{"Agregar proyecto"}</SText> */}
 
-          <DinamicTable ref={ref => this.DinamicTable = ref } loadData={async () => { return await MDL.crm.proyecto.getAll(); }} onSelect={(e) => { console.log("Selected project:", e.row); }} >
+                        <DinamicTable
+                                ref={ref => this.DinamicTable = ref}
+                                loadData={async () => { return await MDL.crm.proyecto.getAll(); }}
+                                onSelect={(e) => {
+                                        console.log("Selected project:", e.row);
+                                        FloatMenu.open({
+                                                e: e.evt,
+                                                label: e.row.nombre,
 
-          <DinamicTable.Col key={"nombre"} label='Nombre'
-           width={120}
-           data={(e) => {
-            return e.row.nombre
-           }} />
-          <DinamicTable.Col key={"descripcion"} label='Descripcion'
-           width={250}
-           data={(e) => {
-            return e.row.descripcion
-           }} />
-          <DinamicTable.Col key={"editar"} label='Editar' width={100} data={(e) => ""}
-           customComponent={e => <SView row card padding={2} onPress={() => {
-            FormRegistroProyecto.open(({
-             defaultData: e.row, onActualizar: (nuevoDato) => {
-              this.DinamicTable.loadData();
-              console.log("Proyecto actualizado:", nuevoDato);
-             }
-            }))
-           }}>
-            <SIcon name='Edit' width={18} />
-            <SView width={4} />
-            <SText center color={STheme.color.green} >{"Actualizar"}</SText>
-           </SView>}
-          />
-          <DinamicTable.Col key={"eliminar"} label='Delete'
-           width={100}
-           data={(e) => ""}
-           customComponent={e => <SView row card padding={2} onPress={() => {
-            console.log("Delete project:", e.row);
-            SSocket.sendPromise({
-             service: "crm",
-             component: "proyecto",
-             type: "editar",
-             data: { ...e.row, estado: 0 }
-            }).then(e => {
-             console.error("❌ Error al recargar proyectos:", e);
-             SNotification.send({ key: "eliminar", title: "eliminado", type: "loading", time: 1000, body: e.error, color: STheme.color.error, })
-             this.DinamicTable.loadData();
-            })
-            alert("✅ Eliminación exitosa del proyecto.");
-           }}>
-            <SIcon name='Delete' width={18} />
-            <SView width={4} />
-            <SText center color={STheme.color.danger} > {"Eliminar"}</SText>
-           </SView>} />
-   </DinamicTable>
-  </SPage >
- }
+                                                options: [
+                                                        {
+                                                                label: "Productos",
+                                                                onPress: () => {
+                                                                      
+                                                                },
+                                                                icon: <SIcon name="producto" fill={STheme.color.text} />,
+                                                        },
+                                                         {
+                                                                label: "Campañas publicitarias",
+                                                                onPress: () => {
+                                                                      
+                                                                },
+                                                                icon: <SIcon name="campana" fill={STheme.color.text} />,
+                                                        },
+                                                        {
+                                                                label: "Editar proyecto",
+                                                                onPress: () => {
+                                                                        FormRegistroProyecto.open(({
+                                                                                defaultData: e.row, onActualizar: (nuevoDato) => {
+                                                                                        this.DinamicTable.loadData();
+                                                                                        console.log("Proyecto actualizado:", nuevoDato);
+                                                                                }
+                                                                        }))
+                                                                },
+                                                                icon: <SIcon name="Edit" fill={STheme.color.text} />,
+                                                        },
+                                                        {
+                                                                label: "Eliminar proyecto",
+                                                                icon: <SIcon name="Delete" fill={STheme.color.text} />,
+                                                                onPress: () => {
+                                                                        SPopup.confirm({
+                                                                                title: "Eliminar Proyecto",
+                                                                                message: "¿Estas seguro de eliminar el proyecto?",
+                                                                                onPress: () => {
+                                                                                        SSocket.sendPromise({
+                                                                                                service: "crm",
+                                                                                                component: "proyecto",
+                                                                                                type: "editar",
+                                                                                                data: { ...e.row, estado: 0 }
+                                                                                        }).then(e => {
+                                                                                                console.error("❌ Error al recargar proyectos:", e);
+                                                                                                SNotification.send({ key: "eliminar", title: "eliminado", type: "loading", time: 1000, body: e.error, color: STheme.color.error, })
+                                                                                                this.DinamicTable.loadData();
+                                                                                        })
+                                                                                }
+                                                                        })
+
+                                                                }
+                                                        },
+
+
+                                                ]
+                                        })
+
+                                }}
+                        >
+
+                                <DinamicTable.Col key={"nombre"} label='Nombre'
+                                        width={120}
+                                        data={(e) => {
+                                                return e.row.nombre
+                                        }} />
+                                <DinamicTable.Col key={"descripcion"} label='Descripcion'
+                                        width={250}
+                                        data={(e) => {
+                                                return e.row.descripcion
+                                        }} />
+                                {/* <DinamicTable.Col key={"editar"} label='Editar' width={100} data={(e) => ""}
+                                        customComponent={e => <SView row card padding={2} onPress={() => {
+                                                FormRegistroProyecto.open(({
+                                                        defaultData: e.row, onActualizar: (nuevoDato) => {
+                                                                this.DinamicTable.loadData();
+                                                                console.log("Proyecto actualizado:", nuevoDato);
+                                                        }
+                                                }))
+                                        }}>
+                                                <SIcon name='Edit' width={18} />
+                                                <SView width={4} />
+                                                <SText center color={STheme.color.green} >{"Actualizar"}</SText>
+                                        </SView>}
+                                />
+                                <DinamicTable.Col key={"eliminar"} label='Delete'
+                                        width={100}
+                                        data={(e) => ""}
+                                        customComponent={e => <SView row card padding={2} onPress={() => {
+                                                console.log("Delete project:", e.row);
+                                                SSocket.sendPromise({
+                                                        service: "crm",
+                                                        component: "proyecto",
+                                                        type: "editar",
+                                                        data: { ...e.row, estado: 0 }
+                                                }).then(e => {
+                                                        console.error("❌ Error al recargar proyectos:", e);
+                                                        SNotification.send({ key: "eliminar", title: "eliminado", type: "loading", time: 1000, body: e.error, color: STheme.color.error, })
+                                                        this.DinamicTable.loadData();
+                                                })
+                                                alert("✅ Eliminación exitosa del proyecto.");
+                                        }}>
+                                                <SIcon name='Delete' width={18} />
+                                                <SView width={4} />
+                                                <SText center color={STheme.color.danger} > {"Eliminar"}</SText>
+                                        </SView>} /> */}
+                        </DinamicTable>
+                        <FloatButtom onPress={() => {
+                                FormRegistroProyecto.open(({
+                                        onRegister: (e) => {
+                                                this.DinamicTable.loadData();
+                                        }
+                                }))
+                        }} />
+                </SPage >
+        }
 }
---
+++ src/Pages/usuario/Components/FormEditarPassword.tsx
@@ -0,0 +1,105 @@
+
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import MDL from '../../../MDL';
+import { DinamicTable } from 'servisofts-table';
+import { SForm, SHr, SIcon, SNotification, SPopup, SText, STheme, SView } from 'servisofts-component';
+import PButtom from '../../../Components/PButtom';
+import { Role } from '../../../MDL/role/types';
+import { Usuario } from '../../../MDL/usuario/types';
+import CryptoJS from 'crypto-js';
+
+
+type FormEditarType = {
+    data: Usuario,
+    onRegister: (e: any) => void,
+    onCancel?: () => void,
+}
+
+export default class FormEditarPassword extends Component<FormEditarType> {
+    static open(props: FormEditarType) {
+        SPopup.open({
+            key: "ppupregistro",
+            content: <SView backgroundColor={STheme.color.background} style={{ borderRadius: 8, maxWidth: 300 }} padding={16} withoutFeedback col={"xs-11"}>
+                <FormEditarPassword {...props} onRegister={(e) => {
+                    SPopup.close("ppupregistro")
+                    if (props.onRegister) props.onRegister(e)
+                }}
+                    onCancel={() => {
+                        SPopup.close("ppupregistro")
+                        if (props.onCancel) props.onCancel()
+                    }}
+                />
+            </SView>
+        })
+    }
+    form: any = null;
+    render() {
+        return <SView center>
+            <SText bold>{"Cambiar Contraseña"}</SText>
+            <SForm
+                ref={(ref: any) => this.form = ref}
+                inputProps={{
+                    col: "xs-12",
+                    separation: 12
+                }}
+                inputs={{
+                    Password: { placeholder: "Ingresa tu contraseña", isRequired: true, type: "password" },
+                    RepPassword: { placeholder: "Repetir Contraseña", type: "password", isRequired: true },
+                }}
+                onSubmit={(e: any) => {
+                    if (e["Password"] != e["RepPassword"]) {
+                        SNotification.send({
+                            title: "Las contraseñas no coinciden",
+                            color: STheme.color.danger,
+                            time: 5000,
+                        })
+                        return null;
+                    }
+                    var password = CryptoJS.MD5(e["Password"]).toString();
+
+                    SNotification.send({
+                        key: "registro",
+                        title: "Cambiando contraseña",
+                        type: "loading",
+                    })
+                    MDL.usuario.editar({
+                        password: password,
+                        key: this.props.data.key,
+
+                    } as any).then((e: any) => {
+                        SNotification.send({
+                            key: "registro",
+                            title: "Contraseña cambiada",
+                            color: STheme.color.success,
+                            time: 5000,
+                        })
+                        if (this.props.onRegister) this.props.onRegister(e)
+
+                    }).catch((e: any) => {
+                        SNotification.send({
+                            key: "registro",
+                            title: "Error al cambiar la contraseña",
+                            body: e,
+                            color: STheme.color.danger,
+                            time: 5000,
+                        })
+                    })
+                }}
+            />
+            <SHr />
+            <SView row col={"xs-12"}>
+                {this.props.onCancel && <>
+                    <PButtom flex type='danger' onPress={() => {
+                        if (this.props.onCancel) this.props.onCancel()
+                    }}>CANCELAR</PButtom>
+                    <SView width={8} />
+                </>}
+
+                <PButtom flex type='primary' onPress={() => {
+                    if (this.form) this.form.submit();
+                }}>GUARDAR</PButtom>
+            </SView>
+        </SView >
+    }
+}
