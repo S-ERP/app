96a4ecd53ebbe826c7d3567314a951555759bdd7
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2025-06-02 18:10:02 -0400

    Update Llamada.tsx

--- src/Pages/crm/Components/Llamada.tsx
+++ src/Pages/crm/Components/Llamada.tsx
@@ -47,49 +47,36 @@ export default class Llamada extends React.Component<{
     return (
       <>
         <style>{`.outer-circle{width:40px;height:40px;border-radius:20px;background-color:rgba(255,255,255,0.3);position:relative;animation:pulse 2.0s ease-out infinite;}.inner-circle{width:20px;height:20px;border-radius:10px;background-color:#FFFFFF;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);box-shadow:0 0 8px #fff;}@keyframes pulse{0%{transform:scale(0.3);opacity:0.8;}70%{transform:scale(2);opacity:0;}100%{transform:scale(0.3);opacity:0;}}`}</style>
-
         <SPage disableScroll hidden>
           {/* <SView col="xs-12" row center>
-          <SView col="xs-3.5" row center>
-            <SView width={160} center style={{ borderColor: "white", borderRadius: 48, borderWidth: 6, backgroundColor: "#8CB1F8" }}>
-              <SView col="xs-12" row center>
-                <SView col="xs-7" row center>
-                  <SView col="xs-9" row>
-                    <SText color="#1D252D">Conectando.</SText>
-                    <SText color="#1D252D" bold>
-                      00:00:00
-                    </SText>
+            <SView col="xs-3.5" row center>
+              <SView width={160} center style={{ borderColor: "white", borderRadius: 48, borderWidth: 6, backgroundColor: "#8CB1F8" }}>
+                <SView col="xs-12" row center>
+                  <SView col="xs-7" row center>
+                    <SView col="xs-9" row>
+                      <SText color="#1D252D">Conectando.</SText>
+                      <SText color="#1D252D" bold>
+                        00:00:00
+                      </SText>
+                    </SView>
                   </SView>
-                </SView>
-                <SView col="xs-5" row center>
-                  <SView width={28} height={28} row style={{ borderRadius: 8, backgroundColor: "#FFFFFF" }} />
+                  <SView col="xs-5" row center>
+                    <SView width={28} height={28} row style={{ borderRadius: 8, backgroundColor: "#FFFFFF" }} />
+                  </SView>
                 </SView>
               </SView>
             </SView>
           </SView>
-        </SView> */}
-
-          {/* <SHr height={40} /> */}
-
+          <SHr height={10} />
           <SView col="xs-12" row center>
             <SView col="xs-3.5" row center>
-              <SView
-                width={180}
-                center
-                style={{
-                  borderColor: "white",
-                  borderRadius: 48,
-                  borderWidth: 6,
-                  backgroundColor: "#8CB1F8",
-                }}
-              >
+              <SView width={180} center style={{ borderColor: "white", borderRadius: 48, borderWidth: 6, backgroundColor: "#8CB1F8" }}>
                 <SView col="xs-12" row center>
                   <SView col="xs-3" row center>
                     <div className="outer-circle">
                       <div className="inner-circle"></div>
                     </div>
                   </SView>
-
                   <SView col="xs-6" row center>
                     <SView col="xs-9" row>
                       <SText color="#1D252D">Llamando.</SText>
@@ -98,47 +85,47 @@ export default class Llamada extends React.Component<{
                       </SText>
                     </SView>
                   </SView>
-
                   <SView col="xs-3" row center>
                     <SView width={28} height={28} row center style={{ borderRadius: 8, backgroundColor: "#FFFFFF" }}>
-                      <SIcon name="microfono" fill="red" height={18}></SIcon>
+                      <SIcon name="microfono" fill="#1D252D" height={18} />
                     </SView>
                   </SView>
                 </SView>
               </SView>
             </SView>
           </SView>
+          <SHr height={10} /> */}
 
-          {/* <SHr height={40} />
-
-        <SView col="xs-12" row center>
-          <SView col="xs-3.5" row center>
-            <SView width={240} center style={{ borderColor: "white", borderRadius: 48, borderWidth: 6, backgroundColor: "#A0F21F" }}>
-              <SView col="xs-12" row center>
-                <SView col="xs-3" row center>
-                  <SView width={28} height={28} row center style={{ borderRadius: 8, backgroundColor: "#FFFFFF" }} />
-                </SView>
-                <SView col="xs-6" row center>
-                  <SView col="xs-9" row>
-                    <SText color="#1D252D">Conectando.</SText>
-                    <SText color="#1D252D" bold>
-                      00:00:00
-                    </SText>
+          <SView col="xs-12" row center>
+            <SView col="xs-3.5" row center>
+              <SView width={320} center style={{ borderColor: "white", borderRadius: 48, borderWidth: 6, backgroundColor: "#A0F21F" }}>
+                <SView col="xs-12" row center>
+                  <SView col="xs-5" row center>
+                    <SView col="xs-8" row>
+                      <SText color="#1D252D">Cliente en linea.</SText>
+                      <SText color="#1D252D" bold>
+                        00:00:00
+                      </SText>
+                    </SView>
                   </SView>
-                </SView>
-                <SView col="xs-3" row center>
-                  <SView col="xs-9" row>
-                    <SText color="#1D252D" bold>
-                      Finalizar llamada
-                    </SText>
+                  <SView col="xs-2" row center border={"transparent"}>
+                    <SView width={28} height={28} row center style={{ borderRadius: 8, backgroundColor: "#FFFFFF" }}>
+                      <SIcon name="microfono" fill="#1D252D" height={18} />
+                    </SView>
+                  </SView>
+                  <SView col="xs-5" row center border={"transparent"}>
+                    <SView col="xs-12" row>
+                      <SView width={120} height={28} row center style={{ borderRadius: 8, backgroundColor: "#1B242C" }}>
+                        <SText color="white" fontSize={12} bold>
+                          Finalizar llamada
+                        </SText>
+                      </SView>
+                    </SView>
                   </SView>
                 </SView>
               </SView>
             </SView>
           </SView>
-        </SView> */}
-
-          {/* <SHr height={150} /> */}
         </SPage>
       </>
     );
