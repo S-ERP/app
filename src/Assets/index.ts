6296501e44ba71577413d8745c4e82e7b0f972f7
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2025-06-02 17:51:27 -0400

    Update Boton llamada

--- src/Assets/index.ts
+++ src/Assets/index.ts
@@ -1,193 +1,182 @@
-import { SAssets } from 'servisofts-component';
+import { SAssets } from "servisofts-component";
 
-import Logo, { ReactComponent as LogoW } from './svg/logo.svg';
-import logoCompleto, { ReactComponent as logoCompletoW } from './svg/logoCompleto.svg';
-import profile2, { ReactComponent as profile2W } from './svg/profile2.svg';
-import IconChecked, { ReactComponent as IconCheckedW } from './svg/iconChecked.svg';
-import IconCheckedOk, { ReactComponent as IconCheckedOkW } from './svg/iconCheckedOk.svg';
-import pregunta1, { ReactComponent as pregunta1W } from './svg/pregunta1.svg';
-import empresa, { ReactComponent as empresaW } from './svg/empresa.svg';
-import empresaBuscar, { ReactComponent as empresaBuscarW } from './svg/empresaBuscar.svg';
-import bg1, { ReactComponent as bg1W } from './svg/bg1.svg';
-import img1, { ReactComponent as img1W } from './svg/img1.svg';
-import construEmpresa, { ReactComponent as construEmpresaW } from './svg/construEmpresa.svg';
-import imgFranja, { ReactComponent as imgFranjaW } from './svg/imgFranja.svg';
-import configurar, { ReactComponent as configurarW } from './svg/configurar.svg';
-import bgBoton, { ReactComponent as bgBotonW } from './svg/bgBoton.svg';
-import cola, { ReactComponent as colaW } from './svg/cola.svg';
-import picture, { ReactComponent as pictureW } from './svg/picture.svg';
-import hand, { ReactComponent as handW } from './svg/hand.svg';
-import chip, { ReactComponent as chipW } from './svg/chip.svg';
-import pinchito, { ReactComponent as pinchitoW } from './svg/pinchito.svg';
-import notaEsquina, { ReactComponent as notaEsquinaW } from './svg/notaEsquina.svg';
-import addUser, { ReactComponent as addUserW } from './svg/addUser.svg';
-import history, { ReactComponent as historyW } from './svg/history.svg';
-import removeNotes, { ReactComponent as removeNotesW } from './svg/removeNotes.svg';
-import remove, { ReactComponent as removeW } from './svg/remove.svg';
-import pass, { ReactComponent as passW } from './svg/pass.svg';
-import ubiPermiso, { ReactComponent as ubiPermisoW } from './svg/ubiPermiso.svg';
-import mapIcon, { ReactComponent as mapIconW } from './svg/mapIcon.svg';
-import widget, { ReactComponent as widgetW } from './svg/widget.svg';
-import ifoto, { ReactComponent as ifotoW } from './svg/ifoto.svg';
-import iback, { ReactComponent as ibackW } from './svg/iback.svg';
-import itema, { ReactComponent as itemaW } from './svg/itema.svg';
+import Logo, { ReactComponent as LogoW } from "./svg/logo.svg";
+import logoCompleto, { ReactComponent as logoCompletoW } from "./svg/logoCompleto.svg";
+import profile2, { ReactComponent as profile2W } from "./svg/profile2.svg";
+import IconChecked, { ReactComponent as IconCheckedW } from "./svg/iconChecked.svg";
+import IconCheckedOk, { ReactComponent as IconCheckedOkW } from "./svg/iconCheckedOk.svg";
+import pregunta1, { ReactComponent as pregunta1W } from "./svg/pregunta1.svg";
+import empresa, { ReactComponent as empresaW } from "./svg/empresa.svg";
+import empresaBuscar, { ReactComponent as empresaBuscarW } from "./svg/empresaBuscar.svg";
+import bg1, { ReactComponent as bg1W } from "./svg/bg1.svg";
+import img1, { ReactComponent as img1W } from "./svg/img1.svg";
+import construEmpresa, { ReactComponent as construEmpresaW } from "./svg/construEmpresa.svg";
+import imgFranja, { ReactComponent as imgFranjaW } from "./svg/imgFranja.svg";
+import configurar, { ReactComponent as configurarW } from "./svg/configurar.svg";
+import bgBoton, { ReactComponent as bgBotonW } from "./svg/bgBoton.svg";
+import cola, { ReactComponent as colaW } from "./svg/cola.svg";
+import picture, { ReactComponent as pictureW } from "./svg/picture.svg";
+import hand, { ReactComponent as handW } from "./svg/hand.svg";
+import chip, { ReactComponent as chipW } from "./svg/chip.svg";
+import pinchito, { ReactComponent as pinchitoW } from "./svg/pinchito.svg";
+import notaEsquina, { ReactComponent as notaEsquinaW } from "./svg/notaEsquina.svg";
+import addUser, { ReactComponent as addUserW } from "./svg/addUser.svg";
+import history, { ReactComponent as historyW } from "./svg/history.svg";
+import removeNotes, { ReactComponent as removeNotesW } from "./svg/removeNotes.svg";
+import remove, { ReactComponent as removeW } from "./svg/remove.svg";
+import pass, { ReactComponent as passW } from "./svg/pass.svg";
+import ubiPermiso, { ReactComponent as ubiPermisoW } from "./svg/ubiPermiso.svg";
+import mapIcon, { ReactComponent as mapIconW } from "./svg/mapIcon.svg";
+import widget, { ReactComponent as widgetW } from "./svg/widget.svg";
+import ifoto, { ReactComponent as ifotoW } from "./svg/ifoto.svg";
+import iback, { ReactComponent as ibackW } from "./svg/iback.svg";
+import itema, { ReactComponent as itemaW } from "./svg/itema.svg";
 
+import Heart, { ReactComponent as HeartW } from "./svg/social/Heart.svg";
+import Comment, { ReactComponent as CommentW } from "./svg/social/Comment.svg";
+import Comment2, { ReactComponent as Comment2W } from "./svg/social/Comment2.svg";
+import addPublicacion, { ReactComponent as addPublicacionW } from "./svg/social/addPublicacion.svg";
 
-import Heart, { ReactComponent as HeartW } from './svg/social/Heart.svg';
-import Comment, { ReactComponent as CommentW } from './svg/social/Comment.svg';
-import Comment2, { ReactComponent as Comment2W } from './svg/social/Comment2.svg';
-import addPublicacion, { ReactComponent as addPublicacionW } from './svg/social/addPublicacion.svg';
+import tareaUser, { ReactComponent as tareaUserW } from "./svg/tarea/user.svg";
+import tarea_engranaje, { ReactComponent as tarea_engranajeW } from "./svg/tarea/engranaje.svg";
+import tarea_close, { ReactComponent as tarea_closeW } from "./svg/tarea/close.svg";
+import tarea_label, { ReactComponent as tarea_labelW } from "./svg/tarea/label.svg";
+import addNotas, { ReactComponent as addNotasW } from "./svg/tarea/addNotas.svg";
+import addTarea, { ReactComponent as addTareaW } from "./svg/tarea/addTarea.svg";
+import eliminar, { ReactComponent as eliminarW } from "./svg/tarea/eliminar.svg";
+import eliminar2, { ReactComponent as eliminar2W } from "./svg/tarea/eliminar2.svg";
+import tarea, { ReactComponent as tareaW } from "./svg/tarea/tarea.svg";
 
-import tareaUser, { ReactComponent as tareaUserW } from './svg/tarea/user.svg';
-import tarea_engranaje, { ReactComponent as tarea_engranajeW } from './svg/tarea/engranaje.svg';
-import tarea_close, { ReactComponent as tarea_closeW } from './svg/tarea/close.svg';
-import tarea_label, { ReactComponent as tarea_labelW } from './svg/tarea/label.svg';
-import addNotas, { ReactComponent as addNotasW } from './svg/tarea/addNotas.svg';
-import addTarea, { ReactComponent as addTareaW } from './svg/tarea/addTarea.svg';
-import eliminar, { ReactComponent as eliminarW } from './svg/tarea/eliminar.svg';
-import eliminar2, { ReactComponent as eliminar2W } from './svg/tarea/eliminar2.svg';
-import tarea, { ReactComponent as tareaW } from './svg/tarea/tarea.svg';
+import emp1, { ReactComponent as emp1W } from "./svg/empresa/emp1.svg";
+import emp2, { ReactComponent as emp2W } from "./svg/empresa/emp2.svg";
+import emp3, { ReactComponent as emp3W } from "./svg/empresa/emp3.svg";
+import emp4, { ReactComponent as emp4W } from "./svg/empresa/emp4.svg";
+import out, { ReactComponent as outW } from "./svg/empresa/out.svg";
+import share, { ReactComponent as shareW } from "./svg/empresa/share.svg";
+import invite, { ReactComponent as inviteW } from "./svg/empresa/invite.svg";
 
-import emp1, { ReactComponent as emp1W } from './svg/empresa/emp1.svg';
-import emp2, { ReactComponent as emp2W } from './svg/empresa/emp2.svg';
-import emp3, { ReactComponent as emp3W } from './svg/empresa/emp3.svg';
-import emp4, { ReactComponent as emp4W } from './svg/empresa/emp4.svg';
-import out, { ReactComponent as outW } from './svg/empresa/out.svg';
-import share, { ReactComponent as shareW } from './svg/empresa/share.svg';
-import invite, { ReactComponent as inviteW } from './svg/empresa/invite.svg';
+import tpAf, { ReactComponent as tpAfW } from "./svg/producto/tpAf.svg";
+import tpGa, { ReactComponent as tpGaW } from "./svg/producto/tpGa.svg";
+import tpIn, { ReactComponent as tpInW } from "./svg/producto/tpIn.svg";
+import tpVs, { ReactComponent as tpVsW } from "./svg/producto/tpVs.svg";
+import productos, { ReactComponent as productosW } from "./svg/producto/productos.svg";
+import add1, { ReactComponent as add1W } from "./svg/producto/add1.svg";
+import deleteAll, { ReactComponent as deleteAllW } from "./svg/producto/deleteAll.svg";
+import menuAll, { ReactComponent as menuAllW } from "./svg/producto/menuAll.svg";
 
-import tpAf, { ReactComponent as tpAfW } from './svg/producto/tpAf.svg';
-import tpGa, { ReactComponent as tpGaW } from './svg/producto/tpGa.svg';
-import tpIn, { ReactComponent as tpInW } from './svg/producto/tpIn.svg';
-import tpVs, { ReactComponent as tpVsW } from './svg/producto/tpVs.svg';
-import productos, { ReactComponent as productosW } from './svg/producto/productos.svg';
-import add1, { ReactComponent as add1W } from './svg/producto/add1.svg';
-import deleteAll, { ReactComponent as deleteAllW } from './svg/producto/deleteAll.svg';
-import menuAll, { ReactComponent as menuAllW } from './svg/producto/menuAll.svg';
+import iCamera, { ReactComponent as iCameraW } from "./svg/scene/iCamera.svg";
+import iEchado, { ReactComponent as iEchadoW } from "./svg/scene/iEchado.svg";
+import iExit, { ReactComponent as iExitW } from "./svg/scene/iExit.svg";
+import iJump, { ReactComponent as iJumpW } from "./svg/scene/iJump.svg";
+import iThree, { ReactComponent as iThreeW } from "./svg/scene/three.svg";
 
-import iCamera, { ReactComponent as iCameraW } from './svg/scene/iCamera.svg';
-import iEchado, { ReactComponent as iEchadoW } from './svg/scene/iEchado.svg';
-import iExit, { ReactComponent as iExitW } from './svg/scene/iExit.svg';
-import iJump, { ReactComponent as iJumpW } from './svg/scene/iJump.svg';
-import iThree, { ReactComponent as iThreeW } from './svg/scene/three.svg';
+import imprimir, { ReactComponent as imprimirW } from "./svg/factura/imprimir.svg";
+import revertir, { ReactComponent as revertirW } from "./svg/factura/revertir.svg";
+import sucursal, { ReactComponent as sucursalW } from "./svg/factura/sucursal.svg";
+import eliminarI, { ReactComponent as eliminarIW } from "./svg/factura/eliminar.svg";
+import adicionar, { ReactComponent as adicionarW } from "./svg/factura/adicionar.svg";
+import whatsappIcon, { ReactComponent as whatsappIconW } from "./svg/whatsapp.svg";
+import invite2, { ReactComponent as invite2W } from "./svg/invite2.svg";
 
+import guion, { ReactComponent as guionW } from "./svg/proyecto/guion.svg";
+import campana, { ReactComponent as campanaW } from "./svg/proyecto/campana.svg";
+import producto, { ReactComponent as productoW } from "./svg/proyecto/producto.svg";
+import microfono, { ReactComponent as microfonoW } from "./svg/microfono.svg";
 
-import imprimir, { ReactComponent as imprimirW } from './svg/factura/imprimir.svg';
-import revertir, { ReactComponent as revertirW } from './svg/factura/revertir.svg';
-import sucursal, { ReactComponent as sucursalW } from './svg/factura/sucursal.svg';
-import eliminarI, { ReactComponent as eliminarIW } from './svg/factura/eliminar.svg';
-import adicionar, { ReactComponent as adicionarW } from './svg/factura/adicionar.svg';
-import whatsappIcon, { ReactComponent as whatsappIconW } from './svg/whatsapp.svg';
-import invite2, { ReactComponent as invite2W } from './svg/invite2.svg';
+import blender from "./svg/blender";
 
-import guion, { ReactComponent as guionW } from './svg/proyecto/guion.svg';
-import campana, { ReactComponent as campanaW } from './svg/proyecto/campana.svg';
-import producto, { ReactComponent as productoW } from './svg/proyecto/producto.svg';
+import drive from "./svg/drive";
 
+export const svg = {
+  Logo: { Native: Logo, Web: LogoW },
+  pregunta1: { Native: pregunta1, Web: pregunta1W },
+  logoCompleto: { Native: logoCompleto, Web: logoCompletoW },
+  profile2: { Native: profile2, Web: profile2W },
+  IconChecked: { Native: IconChecked, Web: IconCheckedW },
+  IconCheckedOk: { Native: IconCheckedOk, Web: IconCheckedOkW },
+  empresa: { Native: empresa, Web: empresaW },
+  empresaBuscar: { Native: empresaBuscar, Web: empresaBuscarW },
+  bg1: { Native: bg1, Web: bg1W },
+  img1: { Native: img1, Web: img1W },
+  construEmpresa: { Native: construEmpresa, Web: construEmpresaW },
+  imgFranja: { Native: imgFranja, Web: imgFranjaW },
+  configurar: { Native: configurar, Web: configurarW },
+  bgBoton: { Native: bgBoton, Web: bgBotonW },
+  cola: { Native: cola, Web: colaW },
+  picture: { Native: picture, Web: pictureW },
+  hand: { Native: hand, Web: handW },
+  chip: { Native: chip, Web: chipW },
+  pinchito: { Native: pinchito, Web: pinchitoW },
+  notaEsquina: { Native: notaEsquina, Web: notaEsquinaW },
+  addUser: { Native: addUser, Web: addUserW },
+  history: { Native: history, Web: historyW },
+  removeNotes: { Native: removeNotes, Web: removeNotesW },
+  remove: { Native: remove, Web: removeW },
+  pass: { Native: pass, Web: passW },
+  ubiPermiso: { Native: ubiPermiso, Web: ubiPermisoW },
+  mapIcon: { Native: mapIcon, Web: mapIconW },
+  widget: { Native: widget, Web: widgetW },
+  ifoto: { Native: ifoto, Web: ifotoW },
+  iback: { Native: iback, Web: ibackW },
+  itema: { Native: itema, Web: itemaW },
 
+  Heart: { Native: Heart, Web: HeartW },
+  Comment: { Native: Comment, Web: CommentW },
+  Comment2: { Native: Comment2, Web: Comment2W },
+  addPublicacion: { Native: addPublicacion, Web: addPublicacionW },
+  tareaUser: { Native: tareaUser, Web: tareaUserW },
+  tareaengranaje: { Native: tarea_engranaje, Web: tarea_engranajeW },
+  tareaclose: { Native: tarea_close, Web: tarea_closeW },
+  tarealabel: { Native: tarea_label, Web: tarea_labelW },
+  addNotas: { Native: addNotas, Web: addNotasW },
+  addTarea: { Native: addTarea, Web: addTareaW },
+  eliminar: { Native: eliminar, Web: eliminarW },
+  eliminar2: { Native: eliminar2, Web: eliminar2W },
+  tarea: { Native: tarea, Web: tareaW },
 
+  emp1: { Native: emp1, Web: emp1W },
+  emp2: { Native: emp2, Web: emp2W },
+  emp3: { Native: emp3, Web: emp3W },
+  emp4: { Native: emp4, Web: emp4W },
+  out: { Native: out, Web: outW },
+  share: { Native: share, Web: shareW },
+  invite: { Native: invite, Web: inviteW },
 
+  tpAf: { Native: tpAf, Web: tpAfW },
+  tpGa: { Native: tpGa, Web: tpGaW },
+  tpIn: { Native: tpIn, Web: tpInW },
+  tpVs: { Native: tpVs, Web: tpVsW },
+  productos: { Native: productos, Web: productosW },
+  add1: { Native: add1, Web: add1W },
+  deleteAll: { Native: deleteAll, Web: deleteAllW },
+  menuAll: { Native: menuAll, Web: menuAllW },
+  ...drive,
 
-import blender from './svg/blender';
+  iCamera: { Native: iCamera, Web: iCameraW },
+  iEchado: { Native: iEchado, Web: iEchadoW },
+  iExit: { Native: iExit, Web: iExitW },
+  iJump: { Native: iJump, Web: iJumpW },
+  iThree: { Native: iThree, Web: iThreeW },
 
+  imprimir: { Native: imprimir, Web: imprimirW },
+  revertir: { Native: revertir, Web: revertirW },
+  sucursal: { Native: sucursal, Web: sucursalW },
+  eliminarI: { Native: eliminarI, Web: eliminarIW },
+  adicionar: { Native: adicionar, Web: adicionarW },
+  whatsapp: { Native: whatsappIcon, Web: whatsappIconW },
+  invite2: { Native: invite2, Web: invite2W },
 
+  guion: { Native: guion, Web: guionW },
+  campana: { Native: campana, Web: campanaW },
+  producto: { Native: producto, Web: productoW },
+  microfono: { Native: microfono, Web: microfonoW },
 
-
-import drive from './svg/drive';
-
-export const svg =  {
-    "Logo": { Native: Logo, Web: LogoW },
-    "pregunta1": { Native: pregunta1, Web: pregunta1W },
-    "logoCompleto": { Native: logoCompleto, Web: logoCompletoW },
-    "profile2": { Native: profile2, Web: profile2W },
-    "IconChecked": { Native: IconChecked, Web: IconCheckedW },
-    "IconCheckedOk": { Native: IconCheckedOk, Web: IconCheckedOkW },
-    "empresa": { Native: empresa, Web: empresaW },
-    "empresaBuscar": { Native: empresaBuscar, Web: empresaBuscarW },
-    "bg1": { Native: bg1, Web: bg1W },
-    "img1": { Native: img1, Web: img1W },
-    "construEmpresa": { Native: construEmpresa, Web: construEmpresaW },
-    "imgFranja": { Native: imgFranja, Web: imgFranjaW },
-    "configurar": { Native: configurar, Web: configurarW },
-    "bgBoton": { Native: bgBoton, Web: bgBotonW },
-    "cola": { Native: cola, Web: colaW },
-    "picture": { Native: picture, Web: pictureW },
-    "hand": { Native: hand, Web: handW },
-    "chip": { Native: chip, Web: chipW },
-    "pinchito": { Native: pinchito, Web: pinchitoW },
-    "notaEsquina": { Native: notaEsquina, Web: notaEsquinaW },
-    "addUser": { Native: addUser, Web: addUserW },
-    "history": { Native: history, Web: historyW },
-    "removeNotes": { Native: removeNotes, Web: removeNotesW },
-    "remove": { Native: remove, Web: removeW },
-    "pass": { Native: pass, Web: passW },
-    "ubiPermiso": { Native: ubiPermiso, Web: ubiPermisoW },
-    "mapIcon": { Native: mapIcon, Web: mapIconW },
-    "widget": { Native: widget, Web: widgetW },
-    "ifoto": { Native: ifoto, Web: ifotoW },
-    "iback": { Native: iback, Web: ibackW },
-    "itema": { Native: itema, Web: itemaW },
-
-
-    "Heart": { Native: Heart, Web: HeartW },
-    "Comment": { Native: Comment, Web: CommentW },
-    "Comment2": { Native: Comment2, Web: Comment2W },
-    "addPublicacion": { Native: addPublicacion, Web: addPublicacionW },
-    "tareaUser": { Native: tareaUser, Web: tareaUserW },
-    "tareaengranaje": { Native: tarea_engranaje, Web: tarea_engranajeW },
-    "tareaclose": { Native: tarea_close, Web: tarea_closeW },
-    "tarealabel": { Native: tarea_label, Web: tarea_labelW },
-    "addNotas": { Native: addNotas, Web: addNotasW },
-    "addTarea": { Native: addTarea, Web: addTareaW },
-    "eliminar": { Native: eliminar, Web: eliminarW },
-    "eliminar2": { Native: eliminar2, Web: eliminar2W },
-    "tarea": { Native: tarea, Web: tareaW },
-
-    "emp1": { Native: emp1, Web: emp1W },
-    "emp2": { Native: emp2, Web: emp2W },
-    "emp3": { Native: emp3, Web: emp3W },
-    "emp4": { Native: emp4, Web: emp4W },
-    "out": { Native: out, Web: outW },
-    "share": { Native: share, Web: shareW },
-    "invite": { Native: invite, Web: inviteW },
-
-    "tpAf": { Native: tpAf, Web: tpAfW },
-    "tpGa": { Native: tpGa, Web: tpGaW },
-    "tpIn": { Native: tpIn, Web: tpInW },
-    "tpVs": { Native: tpVs, Web: tpVsW },
-    "productos": { Native: productos, Web: productosW },
-    "add1": { Native: add1, Web: add1W },
-    "deleteAll": { Native: deleteAll, Web: deleteAllW },
-    "menuAll": { Native: menuAll, Web: menuAllW },
-    ...drive,
-
-    "iCamera": { Native: iCamera, Web: iCameraW },
-    "iEchado": { Native: iEchado, Web: iEchadoW },
-    "iExit": { Native: iExit, Web: iExitW },
-    "iJump": { Native: iJump, Web: iJumpW },
-    "iThree": { Native: iThree, Web: iThreeW },
-
-    "imprimir": { Native: imprimir, Web: imprimirW },
-    "revertir": { Native: revertir, Web: revertirW },
-    "sucursal": { Native: sucursal, Web: sucursalW },
-    "eliminarI": { Native: eliminarI, Web: eliminarIW },
-    "adicionar": { Native: adicionar, Web: adicionarW },
-    "whatsapp": { Native: whatsappIcon, Web: whatsappIconW },
-    "invite2": { Native: invite2, Web: invite2W },
-
-    "guion": { Native: guion, Web: guionW },
-    "campana": { Native: campana, Web: campanaW },
-    "producto": { Native: producto, Web: productoW },
-
-
-    ...blender
-}
+  ...blender,
+};
 const Assets: SAssets = {
-    svg: svg
-}
-
+  svg: svg,
+};
 
 export type IconNamesApp = keyof typeof svg;
-
 
 export default Assets;
--- src/Pages/crm/Components/Llamada.tsx
+++ src/Pages/crm/Components/Llamada.tsx
@@ -1,51 +1,154 @@
 import React from "react";
-import { SPage, SText, SThread, SView } from "servisofts-component";
+import { SHr, SIcon, SPage, SText, SThread, SView } from "servisofts-component";
 import SIP from "../../../Components/SIP";
 import { RTCSession } from "jssip/lib/RTCSession";
 
-export default class Llamada extends React.Component<{ phone: string }> {
-    llamada: RTCSession | null = null;
-    handlePress = () => {
-        if (!this.llamada) {
-            const sip = new SIP();
-            this.llamada = sip.call(this.props.phone, (e: any, evt: any) => {
-                console.log("Evento de llamada:", e, evt);
-            });
-            this.forceUpdate();
-        } else {
-            this.llamada.terminate();
-            this.llamada = null;
-            this.forceUpdate();
-        }
-        // console.log(this.llamada?.)
+export default class Llamada extends React.Component<{
+  phone: string;
+}> {
+  llamada: RTCSession | null = null;
+  handlePress = () => {
+    if (!this.llamada) {
+      const sip = new SIP();
+      this.llamada = sip.call(this.props.phone, (e: any, evt: any) => {
+        console.log("Evento de llamada:", e, evt);
+      });
+      this.forceUpdate();
+    } else {
+      this.llamada.terminate();
+      this.llamada = null;
+      this.forceUpdate();
+    }
+    // console.log(this.llamada?.)
+  };
+  isRun = true;
+  componentDidMount() {
+    this.isRun = true;
+    this.hilo();
+  }
+  componentWillUnmount() {
+    this.isRun = false;
+    if (this.llamada) {
+      this.llamada.terminate();
+      this.llamada = null;
+    }
+  }
+  hilo() {
+    new SThread(1000, "new", false).start(() => {
+      if (!this.isRun) return;
+      if (this.llamada) {
+        this.forceUpdate();
+      }
+      this.hilo();
+    });
+  }
 
-    }
-    isRun = true;
-    componentDidMount() {
-        this.isRun = true;
-        this.hilo();
-    }
-    componentWillUnmount() {
-        this.isRun = false;
-        if (this.llamada) {
-            this.llamada.terminate();
-            this.llamada = null;
-        }
-    }
-    hilo() {
-        new SThread(1000, "new", false).start(() => {
-            if (!this.isRun) return;
-            if (this.llamada) {
-                this.forceUpdate();
-            }
-            this.hilo();
-        });
-    }
-    render() {
-        return <SView onPress={this.handlePress.bind(this)}>
-            {!this.llamada && <SText>{"LLAMAR"}</SText>}
-            {this.llamada && <SText>{"COLGAR" + " " + this.llamada?.start_time}</SText>}
-            <SText >{this.props.phone}</SText>
-        </SView>
-    }
+  render() {
+    return (
+      <>
+        <style>{`.outer-circle{width:40px;height:40px;border-radius:20px;background-color:rgba(255,255,255,0.3);position:relative;animation:pulse 2.0s ease-out infinite;}.inner-circle{width:20px;height:20px;border-radius:10px;background-color:#FFFFFF;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);box-shadow:0 0 8px #fff;}@keyframes pulse{0%{transform:scale(0.3);opacity:0.8;}70%{transform:scale(2);opacity:0;}100%{transform:scale(0.3);opacity:0;}}`}</style>
+
+        <SPage disableScroll hidden>
+          {/* <SView col="xs-12" row center>
+          <SView col="xs-3.5" row center>
+            <SView width={160} center style={{ borderColor: "white", borderRadius: 48, borderWidth: 6, backgroundColor: "#8CB1F8" }}>
+              <SView col="xs-12" row center>
+                <SView col="xs-7" row center>
+                  <SView col="xs-9" row>
+                    <SText color="#1D252D">Conectando.</SText>
+                    <SText color="#1D252D" bold>
+                      00:00:00
+                    </SText>
+                  </SView>
+                </SView>
+                <SView col="xs-5" row center>
+                  <SView width={28} height={28} row style={{ borderRadius: 8, backgroundColor: "#FFFFFF" }} />
+                </SView>
+              </SView>
+            </SView>
+          </SView>
+        </SView> */}
+
+          {/* <SHr height={40} /> */}
+
+          <SView col="xs-12" row center>
+            <SView col="xs-3.5" row center>
+              <SView
+                width={180}
+                center
+                style={{
+                  borderColor: "white",
+                  borderRadius: 48,
+                  borderWidth: 6,
+                  backgroundColor: "#8CB1F8",
+                }}
+              >
+                <SView col="xs-12" row center>
+                  <SView col="xs-3" row center>
+                    <div className="outer-circle">
+                      <div className="inner-circle"></div>
+                    </div>
+                  </SView>
+
+                  <SView col="xs-6" row center>
+                    <SView col="xs-9" row>
+                      <SText color="#1D252D">Llamando.</SText>
+                      <SText color="#1D252D" bold>
+                        00:00:00
+                      </SText>
+                    </SView>
+                  </SView>
+
+                  <SView col="xs-3" row center>
+                    <SView width={28} height={28} row center style={{ borderRadius: 8, backgroundColor: "#FFFFFF" }}>
+                      <SIcon name="microfono" fill="red" height={18}></SIcon>
+                    </SView>
+                  </SView>
+                </SView>
+              </SView>
+            </SView>
+          </SView>
+
+          {/* <SHr height={40} />
+
+        <SView col="xs-12" row center>
+          <SView col="xs-3.5" row center>
+            <SView width={240} center style={{ borderColor: "white", borderRadius: 48, borderWidth: 6, backgroundColor: "#A0F21F" }}>
+              <SView col="xs-12" row center>
+                <SView col="xs-3" row center>
+                  <SView width={28} height={28} row center style={{ borderRadius: 8, backgroundColor: "#FFFFFF" }} />
+                </SView>
+                <SView col="xs-6" row center>
+                  <SView col="xs-9" row>
+                    <SText color="#1D252D">Conectando.</SText>
+                    <SText color="#1D252D" bold>
+                      00:00:00
+                    </SText>
+                  </SView>
+                </SView>
+                <SView col="xs-3" row center>
+                  <SView col="xs-9" row>
+                    <SText color="#1D252D" bold>
+                      Finalizar llamada
+                    </SText>
+                  </SView>
+                </SView>
+              </SView>
+            </SView>
+          </SView>
+        </SView> */}
+
+          {/* <SHr height={150} /> */}
+        </SPage>
+      </>
+    );
+  }
+
+  // render() {
+  //     return <SView onPress={this.handlePress.bind(this)}>
+  //         {!this.llamada && <SText>{"LLAMAR 5555"}</SText>}
+  //         {this.llamada && <SText>{"COLGAR SS" + " " + this.llamada?.start_time+ "DDD"}</SText>}
+  //         <SText >{this.props.phone}</SText>
+  //     </SView>
+  // }
 }
