30aa4f0c58332b3138c68983c04ced45f6eae9d1
rickypazd <35882906+rickypazd@users.noreply.github.com>
2025-06-03 04:11:46 -0400

    Update type.ts

--- src/MDL/crm/type.ts
+++ src/MDL/crm/type.ts
@@ -1,58 +1,74 @@
 
 export type Proyecto = {
- key: string;
- key_usuario: string;
- fecha_on: string;
- estado: number;
- key_empresa: string;
- descripcion: string;
- nombre: string;
- fecha_inicio?: string;
- fecha_fin?: string;
+    key: string;
+    key_usuario: string;
+    fecha_on: string;
+    estado: number;
+    key_empresa: string;
+    descripcion: string;
+    nombre: string;
+    fecha_inicio?: string;
+    fecha_fin?: string;
 };
 export type Campana = {
- key: string;
- key_usuario: string;
- fecha_on: string;
- estado: number;
- key_proyecto: string;
- descripcion: string;
- nombre: string;
+    key: string;
+    key_usuario: string;
+    fecha_on: string;
+    estado: number;
+    key_proyecto: string;
+    descripcion: string;
+    nombre: string;
 };
 
 export type Cliente = {
- key: string;
- key_usuario: string;
- fecha_on: string;
- estado: number;
- key_empresa: string;
- descripcion: string;
- key_servicio: string;
- nombres: string;
- apellidos: string;
- nit: string;
- razon_social: string;
- correo: string;
- telefono: string;
- lat: string;
- lng: string;
- direccion: string;
- fecha_nacimiento: Date;
- sexo: string;
+    key: string;
+    key_usuario: string;
+    fecha_on: string;
+    estado: number;
+    key_empresa: string;
+    descripcion: string;
+    key_servicio: string;
+    nombres: string;
+    apellidos: string;
+    nit: string;
+    razon_social: string;
+    correo: string;
+    telefono: string;
+    lat: string;
+    lng: string;
+    direccion: string;
+    fecha_nacimiento: Date;
+    sexo: string;
 };
 
 export type TipoMovimientoLead = {
- key: string;
- key_usuario: string;
- fecha_on: string;
- estado: number;
+    key: string;
+    key_usuario: string;
+    fecha_on: string;
+    estado: number;
 
- key_empresa: string;
- titulo: string;
- tipo: string;
- descripcion: string;
+    key_empresa: string;
+    titulo: string;
+    tipo: string;
+    descripcion: string;
 };
 
+export type StatesClienteProyecto = "nuevo"
+    | "rellamada"
+    | "llamada_fallida"
+    | "en_espera"
+    | "confirmado"
+    | "cancelado"
+    | "enviado"
+    | "en_espera_pago"
+    | "en_espera_pago_sin_respuesta"
+    | "en_espera_pago_rellamada"
+    | "rechazo"
+    | "pagado"
+    | "devuelto"
+    | "spam"
+    | "double"
+
 export type EventListener = {
- type: "onChange",
+    type: "onChange",
 }
