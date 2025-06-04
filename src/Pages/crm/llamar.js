dd0cdc44ce69b70f3f3c6438f8d092a84153beef
rickypazd <35882906+rickypazd@users.noreply.github.com>
2025-06-04 01:48:23 -0400

    Update llamar.js

--- src/Pages/crm/llamar.js
+++ src/Pages/crm/llamar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { SHr, SLoad, SPage, SText } from 'servisofts-component';
+import { SHr, SLoad, SNavigation, SPage, SText } from 'servisofts-component';
+import SSocket from 'servisofts-socket';
+import Model from '../../Model';
 
 export default class llamar extends Component {
     constructor(props) {
@@ -9,6 +11,21 @@ export default class llamar extends Component {
         };
     }
 
+    componentDidMount() {
+        SSocket.sendPromise({
+            service: "crm",
+            component: "cliente_proyecto",
+            type: "get_siguiente",
+            key_empresa: Model.empresa.Action.getKey(),
+        }).then(e => {
+            const first = e.data[0];
+            if(!first) return;
+            SNavigation.replace("/crm/plantilla", { key: first.key })
+            console.log("Siguiente Lead:", e);
+        }).catch(error => {
+            console.error("Error al obtener el siguiente lead:", error);
+        });
+    }
     render() {
         return <SPage title={"Llamar"} center>
             <SLoad />
