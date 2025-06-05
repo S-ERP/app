c7ecc4393fce9cdc74acff2472b6c8579462892e
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2025-06-05 05:42:58 -0400

    Update Etiqueta.js

--- src/Pages/crm/Components/Etiqueta.js
+++ src/Pages/crm/Components/Etiqueta.js
@@ -19,7 +19,7 @@ export default class Etiqueta extends Component<{ tipo_leads: string }> {
             backgroundColor: (MDL.crm.clienteProyecto.stageColor[this.props.tipo_leads] ?? STheme.color.text),
             ...(this.props.style ?? {})
         }}>
-         <SText fontSize={10} color={STheme.color.text} center>{MDL.crm.clienteProyecto.stageNombre[this.props.tipo_leads]}</SText>
+         <SText fontSize={size} color={STheme.color.text} center>{MDL.crm.clienteProyecto.stageNombre[this.props.tipo_leads]}</SText>
         </SView>
     }
 }
