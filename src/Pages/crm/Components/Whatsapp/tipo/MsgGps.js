c58396d78253c082f2e36458a0aea2265c214e39
rickypazd <35882906+rickypazd@users.noreply.github.com>
2025-06-12 05:57:38 -0400

    sync

--- src/Pages/crm/Components/Whatsapp/tipo/MsgGps.js
+++ src/Pages/crm/Components/Whatsapp/tipo/MsgGps.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { View } from "react-native";
 import { SDate, SHr, SImage, SInput, SList, SLoad, SMath, SNavigation, SText, STheme, SThread, SView, SIcon } from "servisofts-component";
 import SSocket from "servisofts-socket";
+import HoraLabel from "../Comp/HoraLabel";
 
 export default class MsgGps extends Component {
     constructor(props) {
@@ -29,22 +30,26 @@ export default class MsgGps extends Component {
             <View
                 style={{
                     backgroundColor: this.props.color,
-                    borderRadius: 12, padding: 6, marginHorizontal: 10,
-                    width: "80%"
+                    borderRadius: 12, padding: 4,
+                    marginHorizontal: 10,
+                    width: "75%"
                 }}>
                 {/* <SText color={"white"} fontSize={14}>{texto}</SText> */}
                 {/* <SImage src={}/> */}
-                <SView style={{ width: "100%", height: 200, borderRadius: 12, overflow: "hidden", backgroundColor: STheme.color.card }} onPress={(e)=>{
+                <SView style={{ width: "100%", height: 150, borderRadius: 12, overflow: "hidden", backgroundColor: STheme.color.card }} onPress={(e) => {
                     console.log(this.props.mensaje)
                 }}>
-                    {/* <SImage src={this.props.mensaje.mediaData} style={{ resizeMode: "cover", width: "100%", height: "100%" }} /> */}
-                    <SText>{JSON.stringify(this.props.mensaje)}</SText>
+                    {/* <SImage src={"data:img/png;base64," + this.props.mensaje.body} style={{ resizeMode: "cover", width: "100%", height: "100%" }} /> */}
+                    <SImage src={this.props.mensaje.mediaData} style={{ resizeMode: "cover", width: "100%", height: "100%" }} />
+                    {/* <SText>{JSON.stringify(this.props.mensaje)}</SText> */}
                 </SView>
                 <SHr />
                 <SView padding={4}>
-                    <SText>{"Location TODO"}</SText>
+                    <SText>{this.props.mensaje?.location?.address}</SText>
+                    <SView style={{ alignItems: "flex-end" }}>
+                        <HoraLabel style={{}} mesaje={this.props.mensaje} />
+                    </SView>
                 </SView>
-                <SHr />
             </View>
         );
     }
