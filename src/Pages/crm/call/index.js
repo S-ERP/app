fc5c3f4bc83f76482e6065e0efb4a58d7b003287
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2025-06-12 00:44:12 -0400

    Update index.js

--- src/Pages/crm/call/index.js
+++ src/Pages/crm/call/index.js
@@ -117,11 +117,16 @@ export default class index extends Component {
         return (
             <SView col={"xs-12"} row style={{ backgroundColor: STheme.color.card, padding: 15, borderBottomWidth: 1, borderBottomColor: "green" }}>
                 <SView col={"xs-8"} row style={{ justifyContent: "flex-start" }}>
+
                     <SView width={40} height={40} style={{ borderRadius: 100, overflow: "hidden" }}>
-                        <SImage enablePreview src={SSocket.api.root + "usuario/" + this.state?.data?.cliente.key_u} style={{ resizeMode: "cover" }} />
+                        <SImage enablePreview src={"https://avatars.githubusercontent.com/u/69025139?v=4"} style={{ resizeMode: "cover" }} />
+                        {/* <SImage enablePreview src={SSocket.api.root + "usuario/" + this.state?.data?.cliente.key_u} style={{ resizeMode: "cover" }} /> */}
                     </SView>
-                    <SText color={"white"} fontSize={18}> </SText>
-                    <SText color={"white"} fontSize={18} bold>{this.state?.data?.cliente?.telefono}</SText>
+
+                    <SView flex row style={{ marginLeft:16 }}>
+                        <SText col={"xs-12"} color={"white"} fontSize={14} bold>{this.state?.data?.cliente?.nombres}</SText>
+                        <SText col={"xs-12"} color={"white"} fontSize={12} bold>{this.state?.data?.cliente?.telefono}</SText>
+                    </SView>
                 </SView>
                 <SView col={"xs-4"} row center style={{ justifyContent: "flex-end" }}>
                     <SIcon name='drive-menu' fill='white' width={18} height={18} />
@@ -173,21 +178,34 @@ export default class index extends Component {
     }
 
 
-    renderBarraEntrada( ) {
+    renderBarraEntrada() {
         return (<SView col={"xs-12"} row style={{ backgroundColor: STheme.color.card, padding: 15, bottom: 0, left: 0, right: 0 }}>
             <SView style={{ marginRight: 15 }}><SIcon name='add1' fill='white' width={18} /></SView>
             <SView style={{ marginRight: 15 }}><SIcon name='addTarea' fill='white' width={18} /></SView>
             <SView flex style={{ marginRight: 15 }}>
-                <SInput ref={ref =>this.campos = ref}  placeholder="Escribe un mensaje" placeholderTextColor="#8696a0" style={{ backgroundColor: "#2a3942", borderRadius: 20, paddingHorizontal: 20, color: "white", borderWidth: 0 }} />
+                <SInput ref={ref => this.campos = ref} placeholder="Escribe un mensaje" placeholderTextColor="#8696a0" style={{ backgroundColor: "#2a3942", borderRadius: 20, paddingHorizontal: 20, color: "white", borderWidth: 0 }}
+                    onKeyPress={(e) => {
+                        if (e.key === 'Enter') {
+
+
+                        e.preventDefault(); // Evita el salto de línea al presionar Enter
+                            MDL.whatsapp.send({
+                                phone: this.state?.data?.cliente?.telefono,
+                                message: this.campos.getValue(),
+                            });
+                            console.log("aqui " + this.state?.data?.cliente?.telefono + " mensaje " + this.campos.getValue())
+                            this.campos.setValue("");
+                    }
+                }}
+                />
             </SView>
             <SView onPress={() => {
                 MDL.whatsapp.send({
                     phone: this.state?.data?.cliente?.telefono,
                     message: this.campos.getValue(),
                 });
-
-                console.log("aqui " + this.state?.data?.cliente?.telefono+ " mensaje "+ this.campos.getValue())
-
+                console.log("aqui " + this.state?.data?.cliente?.telefono + " mensaje " + this.campos.getValue())
+                this.campos.setValue("");
             }}
             ><SIcon name='MessageSend' fill='white' width={18} /></SView>
         </SView>
