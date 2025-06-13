246b49e13db242099ba25f474ef5920ac0c39566
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2025-06-13 06:54:29 -0400

    Update whatsapp_devices.js

--- src/Pages/whatsapp_devices.js
+++ src/Pages/whatsapp_devices.js
@@ -4,6 +4,7 @@ import { SPage, SView, SText, STheme, SIcon, SPopup, SInput, SButtom, SHr, SNoti
 import { DinamicTable } from 'servisofts-table';
 import FloatButtom from '../Components/FloatButtom';
 import MDL from '../MDL';
+import Config from '../Config';
 
 
 
@@ -32,70 +33,108 @@ class WhatsappDevices extends Component {
     formulario = (data = null) => {
         const isEdit = !!data;
         let descripcionRef;
-
         return SPopup.open({
             key: "formulario_dispositivo",
             content: (
-                <SView backgroundColor={STheme.color.background} style={{ borderRadius: 8, maxWidth: 300 }} padding={16} withoutFeedback col={"xs-11"}  >
-                    <SView center>
+                <SView backgroundColor={STheme.color.background} style={{ borderRadius: 8, maxWidth: 300 }} padding={16} withoutFeedback col={"xs-11"}    >
+                    {/* <SView center>
+                    </SView> */}
 
-                        <SText fontSize={16} bold> {isEdit ? "Editar Dispositivo" : "Registrar Dispositivo"} </SText>
-                        <SHr height={16} />
-                        <SInput
-                            label="Descripción"
-                            defaultValue={data?.descripcion || ""}
-                            ref={ref => descripcionRef = ref}
-                            style={{
-                                height: 40,
-                                borderRadius: 4,
-                                backgroundColor: STheme.color.lightGray + "30",
-                                textAlign: "center",
-                                color: STheme.color.text,
+
+                    {/* Registrar nuevo dispositivo WhatsApp
+                        Complete la información y escanee el código QR para sincronizar un nuevo dispositivo de WhatsApp Business. */}
+
+                    <SText fontSize={16} bold>{isEdit ? "Actualizar dispositivo WhatsApp" : "Registrar nuevo dispositivo WhatsApp"} </SText>
+
+                    <SHr height={8} />
+                    <SText fontSize={12}>Complete la información y escanee el código QR para sincronizar un nuevo dispositivo de WhatsApp Business.</SText>
+                    <SHr height={16} />
+                    <SInput
+                        label="Descripción"
+                        placeholder={"Nombre del dispositivo"}
+                        defaultValue={data?.descripcion || ""}
+                        ref={ref => descripcionRef = ref}
+                        style={{
+                            height: 40,
+                            borderRadius: 4,
+                            backgroundColor: STheme.color.lightGray + "30",
+                            // textAlign: "center",
+                            color: STheme.color.text,
+                        }}
+                    />
+
+                    <SInput
+                        label="Número"
+                        placeholder={"+591 XXXXXXXX"}
+                        defaultValue={data?.descripcion || ""}
+                        ref={ref => telefonoRef = ref}
+                        style={{
+                            height: 40,
+                            borderRadius: 4,
+                            backgroundColor: STheme.color.lightGray + "30",
+                            // textAlign: "center",
+                            color: STheme.color.text,
+                        }}
+                    />
+
+                    <SInput
+                        label="Empresa"
+                        placeholder={"Seleccione empresa"}
+
+                        defaultValue={data?.key_empresa || ""}
+                        ref={ref => telefonoRef = ref}
+                        style={{
+                            height: 40,
+                            borderRadius: 4,
+                            backgroundColor: STheme.color.lightGray + "30",
+                            // textAlign: "center",
+                            color: STheme.color.text,
+                        }}
+                    />
+                    <SHr height={32} />
+                    <SView row center>
+                        <SButtom
+                            type={"danger"}
+                            onPress={() => {
+                                SPopup.close("formulario_dispositivo");
                             }}
-                        />
-                        <SHr height={24} />
-                        <SHr height={32} />
-                        <SView row>
-                            <SButtom
-                                type={isEdit ? "outline" : "success"}
-                                onPress={async () => {
-                                    const descripcion = descripcionRef.getValue();
-                                    if (!descripcion) {
-                                        SNotification.send({
-                                            title: "Error",
-                                            body: "Debe ingresar una descripción.",
-                                            color: STheme.color.danger,
-                                        });
-                                        return;
-                                    }
-                                    if (isEdit) {
-                                        await MDL.whatsapp.device.edit(data.key, { descripcion });
-                                    } else {
-                                        await MDL.whatsapp.device.registrar({ descripcion });
-                                    }
+                        >
+                            {"Cancelar"}
+                        </SButtom>
+                        <SView width={16} />
+                        <SButtom
+                            type={isEdit ? "outline" : "outline"}
+                            onPress={async () => {
+                                const descripcion = descripcionRef.getValue();
+                                if (!descripcion) {
                                     SNotification.send({
-                                        title: isEdit ? "Dispositivo actualizado" : "Dispositivo registrado",
-                                        body: "",
-                                        color: STheme.color.success,
-                                        time: 4000,
+                                        title: "Error",
+                                        body: "Debe ingresar una descripción.",
+                                        color: STheme.color.danger,
                                     });
-                                    this.forceUpdate();
-                                }}
-                            >
-                                {isEdit ? "Actualizar" : "Registrar"}
-                            </SButtom>
-                            <SView width={16} />
-                            <SButtom
-                                type={"danger"}
-                                onPress={() => {
-                                    SPopup.close("formulario_dispositivo");
-                                }}
-                            >
-                                {"Cancelar"}
-                            </SButtom>
-                        </SView>
-                        <SView col={"xs-12"} />
+                                    return;
+                                }
+                                if (isEdit) {
+                                    await MDL.whatsapp.device.edit(data.key, { descripcion });
+                                } else {
+                                    await MDL.whatsapp.device.registrar({ descripcion });
+                                }
+                                SNotification.send({
+                                    title: isEdit ? "Dispositivo actualizado" : "Dispositivo registrado",
+                                    body: "",
+                                    color: STheme.color.success,
+                                    time: 4000,
+                                });
+                                this.DinamicTable.loadData();
+
+                                SPopup.close("formulario_dispositivo");
+
+                            }}
+                        >
+                            {isEdit ? "Actualizar" : "Registrar"}
+                        </SButtom>
                     </SView>
+                    <SView col={"xs-12"} />
                 </SView>
             )
         });
@@ -105,9 +144,19 @@ class WhatsappDevices extends Component {
         return (
             <SPage title="Dispositivos WhatsApp">
                 <DinamicTable
+                    ref={ref => this.DinamicTable = ref}
                     loadData={async () => await MDL.whatsapp.device.getAll()}
                     key="id"
                     language="es"
+
+
+                    //  key='index' textStyle={{ fontSize: 10, color: STheme.color.lightGray }}
+                    colors={Config.table.colors()}
+                    cellStyle={Config.table.cellStyle()}
+                    textStyle={Config.table.textStyle()}
+                    selectType='single'
+                // ref={ref => this.DinamicTable = ref} loadData={async () => { return await MDL.whatsapp.device.getAll(); }} onSelect={(e) => { console.log("Selected project:", e.row); }}
+
                 >
                     <DinamicTable.Col key="index" label="N°" width={40} data={e => e.index + 1} />
                     <DinamicTable.Col key="key" label="Key" width={200} data={e => e.row.key} />
@@ -119,7 +168,7 @@ class WhatsappDevices extends Component {
                     <DinamicTable.Col key={"editar"} label='Editar' width={100} data={() => ""}
                         customComponent={e => (
                             <SView row card padding={2} onPress={() => this.formulario(e?.row)}>
-                                <SIcon name='Edit' width={18} />
+                                <SIcon name='Edit' fill='blue' width={18} />
                                 <SView width={4} />
                                 <SText center color={STheme.color.green}>
                                     {"Actualizar"}
@@ -134,7 +183,10 @@ class WhatsappDevices extends Component {
                                 SPopup.confirm({
                                     title: "Esta seguro que quiere eliminar?",
                                     message: "Se le enviara a la lista de compras.",
-                                    onPress: () => { MDL.whatsapp.device.edit(e?.row?.key, { estado: 0 }) }
+                                    onPress: () => {
+                                        MDL.whatsapp.device.edit(e?.row?.key, { estado: 0 })
+                                        this.DinamicTable.loadData();
+                                    }
                                 })
                             }}>
                                 <SIcon name='Delete' width={18} />
