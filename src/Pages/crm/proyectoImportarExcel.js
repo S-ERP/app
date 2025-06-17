91f5714c7d7ca4253a4c1778053bb729191a0a64
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2025-06-17 00:05:41 -0400

    Update proyectoImportarExcel.js

--- src/Pages/crm/proyectoImportarExcel.js
+++ src/Pages/crm/proyectoImportarExcel.js
@@ -3,7 +3,16 @@ import { SPage, SView, SIcon, SText, STable, STheme, SLoad, SNavigation } from '
 import FileChooser from '../../Components/SUpload/FileChooser';
 import * as XLSX from "xlsx";
 
-export default class Root extends Component {
+
+// import React, { Component } from 'react';
+// import {
+//     SPage, SView, SIcon, SText,
+//     STable, STheme, SNavigation
+// } from 'servisofts-component';
+// import FileChooser from '../../Components/SUpload/FileChooser';
+// import * as XLSX from "xlsx";
+
+export default class ProyectoImportarExcel extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -21,15 +30,17 @@ export default class Root extends Component {
             reader.onload = (e) => {
                 const data = new Uint8Array(e.target.result);
                 const workbook = XLSX.read(data, { type: "array" });
-                const firstSheetName = workbook.SheetNames[0];
-                const worksheet = workbook.Sheets[firstSheetName];
+                const worksheet = workbook.Sheets[workbook.SheetNames[0]];
                 const jsonData = XLSX.utils.sheet_to_json(worksheet, { defval: "" });
+
                 const filtrado = jsonData
                     .map((row, index) => ({
                         key: `row_${index}`,
                         index: index + 1,
-                        nombres: row["Nombre completo"] || "Sin nombre",
-                        telefono: row["Teléfono"] || ""
+                        cliente: row["nombres"] || row["clienteqqqqqqqq"] || "Sin nombre",
+                        telefono: row["telefono"] || row["telefonossswwww"] || "Sin teléfono",
+                        key_campana: this.key_campana,
+                        key_proyecto: this.key_proyecto
                     }))
                     .filter(e => e.telefono && e.telefono.toString().length >= 8);
 
@@ -40,57 +51,69 @@ export default class Root extends Component {
         });
     };
 
+    renderImportButton = () => (
+        <SView
+            backgroundColor='white'
+            style={{
+                position: "absolute",
+                top: 20,
+                right: "30%",
+                borderRadius: 8,
+                elevation: 4,
+                padding: 8,
+                shadowColor: "#00000040"
+            }}
+            width={160}
+            height={50}
+            center
+            onPress={this.handleExcelImport}
+        >
+            <SView row center>
+                <SIcon name='Reload' width={18} />
+                <SView width={10} />
+                <SText color='black' fontSize={16}>Importar Excel</SText>
+            </SView>
+        </SView>
+    );
+
+    renderTable = () => {
+        if (!this.state.data) {
+            return (
+                <SView center flex>
+                    <SText color={STheme.color.lightGray}>📂 Aún no se ha importado ningún archivo</SText>
+                </SView>
+            );
+        }
+
+        return (
+            <STable
+                header={[
+                    { key: "index", label: "#", width: 40 },
+                    { key: "cliente", label: "Nombre completo", width: 250 },
+                    { key: "telefono", label: "Teléfono", width: 250 },
+                    {
+                        key: "key_campana",
+                        label: "Campaña",
+                        width: 320,
+                        render: (val, row) => row.key_campana || "Sin campaña"
+                    },
+                    {
+                        key: "key_proyecto",
+                        label: "Proyecto",
+                        width: 320,
+                        render: (val, row) => row.key_proyecto || "Sin proyecto"
+                    }
+                ]}
+                data={this.state.data}
+            />
+        );
+    };
+
     render() {
         return (
             <SPage title="Importar contactos desde Excel" disableScroll>
-                {this.state.data ? (
-                    <STable
-                        header={[
-                            { key: "index", label: "#", width: 40 },
-                            { key: "nombres", label: "Nombre completo", width: 250 },
-                            { key: "telefono", label: "Teléfono", width: 250 },
-
-                            {
-                                key: "key_campana",
-                                label: "key_campana",
-                                width: 320,
-                                render: key => this.key_campana || "Sin key_campana"
-                            },
-                            {
-                                key: "key_proyecto",
-                                label: "key_proyecto",
-                                width: 320,
-                                render: key => this.key_proyecto || "Sin proyecto"
-                            }
-                        ]}
-                        data={this.state.data}
-                    />
-                ) : (
-                    <SView center flex>
-                        <SText color={STheme.color.lightGray}>📂 Aún no se ha importado ningún archivo</SText>
-                    </SView>
-                )}
-
-                <SView
-                    backgroundColor='white'
-                    style={{
-                        position: "absolute",
-                        top: 20,
-                        right: "30%",
-                        borderRadius: 4,
-                        overflow: "hidden",
-                    }}
-                    width={140}
-                    height={50}
-                    center
-                    onPress={this.handleExcelImport}
-                >
-                    <SView row>
-                        <SIcon name='Reload' width={15} />
-                        <SView width={10} />
-                        <SText color='black' fontSize={18}>Importar</SText>
-                    </SView>
-                </SView>
+                {this.renderTable()}
+                {this.renderImportButton()}
             </SPage>
         );
     }
