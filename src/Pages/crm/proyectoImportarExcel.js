3769891f8b66541d4003b7829e938bcbaccd6168
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2025-06-17 01:23:31 -0400

    Update proyectoImportarExcel.js

--- src/Pages/crm/proyectoImportarExcel.js
+++ src/Pages/crm/proyectoImportarExcel.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
-import { SPage, SView, SIcon, SText, STable, STheme, SLoad, SNavigation } from 'servisofts-component';
+import { SPage, SView, SIcon, SText, STable, STheme, SLoad, SNavigation, SPopup, SInput } from 'servisofts-component';
 import FileChooser from '../../Components/SUpload/FileChooser';
 import * as XLSX from "xlsx";
 
-
-// import React, { Component } from 'react';
-// import {
-//     SPage, SView, SIcon, SText,
-//     STable, STheme, SNavigation
-// } from 'servisofts-component';
-// import FileChooser from '../../Components/SUpload/FileChooser';
-// import * as XLSX from "xlsx";
 
 export default class ProyectoImportarExcel extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: null
+            data: null,
+            columnasDetectadas: [],
+            mapeo: {}
         };
         this.key_campana = SNavigation.getParam("key_campana");
         this.key_proyecto = SNavigation.getParam("key_proyecto");
@@ -30,90 +24,137 @@ export default class ProyectoImportarExcel extends Component {
             reader.onload = (e) => {
                 const data = new Uint8Array(e.target.result);
                 const workbook = XLSX.read(data, { type: "array" });
-                const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+                const firstSheetName = workbook.SheetNames[0];
+                const worksheet = workbook.Sheets[firstSheetName];
                 const jsonData = XLSX.utils.sheet_to_json(worksheet, { defval: "" });
-
-                const filtrado = jsonData
-                    .map((row, index) => ({
-                        key: `row_${index}`,
-                        index: index + 1,
-                        cliente: row["nombres"] || row["clienteqqqqqqqq"] || "Sin nombre",
-                        telefono: row["telefono"] || row["telefonossswwww"] || "Sin teléfono",
-                        key_campana: this.key_campana,
-                        key_proyecto: this.key_proyecto
-                    }))
-                    .filter(e => e.telefono && e.telefono.toString().length >= 8);
-
-                console.log("📊 Excel importado:", filtrado);
-                this.setState({ data: filtrado });
+                const columnas = Object.keys(jsonData[0] || {});
+                this.setState({ excelData: jsonData, columnasDetectadas: columnas }, this.openMappingPopup);
             };
             reader.readAsArrayBuffer(files[0]);
         });
     };
 
-    renderImportButton = () => (
-        <SView
-            backgroundColor='white'
-            style={{
-                position: "absolute",
-                top: 20,
-                right: "30%",
-                borderRadius: 8,
-                elevation: 4,
-                padding: 8,
-                shadowColor: "#00000040"
-            }}
-            width={160}
-            height={50}
-            center
-            onPress={this.handleExcelImport}
-        >
-            <SView row center>
-                <SIcon name='Reload' width={18} />
-                <SView width={10} />
-                <SText color='black' fontSize={16}>Importar Excel</SText>
-            </SView>
-        </SView>
-    );
+    openMappingPopup = () => {
+        const { columnasDetectadas, mapeo = {} } = this.state;
+        const tempMapeo = { ...mapeo };
 
-    renderTable = () => {
-        if (!this.state.data) {
-            return (
-                <SView center flex>
-                    <SText color={STheme.color.lightGray}>📂 Aún no se ha importado ningún archivo</SText>
+        SPopup.open({
+            key: "popup-mapeo",
+            content: (
+                     <SView backgroundColor={STheme.color.background} style={{ borderRadius: 8, maxWidth: 300 }} padding={16} withoutFeedback col={"xs-11"}    >
+
+                {/* <SView width={350} padding={20} backgroundColor={STheme.color.card} borderRadius={10}> */}
+                    <SText bold fontSize={18}>📋 Mapear columnas</SText>
+                    <SView height={12} />
+                    {columnasDetectadas.map((col, i) => (
+                        <SView key={col} marginBottom={12}>
+                            <SText color={STheme.color.text} fontSize={14}>{col}</SText>
+                            <SInput
+                                type="select"
+                                defaultValue={tempMapeo[col] ?? "none"}
+                                center
+                                options={[
+                                    { key: "none", content: "-" },
+                                    // { key: "none", content: "--- Ignorar ---" },
+                                    // { key: "index", content: "Index" },
+                                    { key: "cliente", content: "Cliente" },
+                                    { key: "telefono", content: "Teléfono" },
+                                ]}
+                                onChangeText={(val) => {
+                                    tempMapeo[col] = val;
+
+                                }}
+                                style={{ textAlign: "center" }}
+                            />
+                        </SView>
+                    ))}
+                    <SView height={20} />
+                    <SView row center>
+                        <SView flex />
+                        <SView
+                            backgroundColor={STheme.color.primary}
+                            padding={10}
+                            borderRadius={8}
+                            onPress={() => {
+                                this.setState({ mapeo: tempMapeo }, this.applyMapping);
+                                SPopup.close("popup-mapeo");
+                            }}
+                        >
+                            <SText color={"#fff"}>Aceptar</SText>
+                        </SView>
+                    </SView>
                 </SView>
-            );
-        }
+            )
+        });
+    };
 
-        return (
-            <STable
-                header={[
-                    { key: "index", label: "#", width: 40 },
-                    { key: "cliente", label: "Nombre completo", width: 250 },
-                    { key: "telefono", label: "Teléfono", width: 250 },
-                    {
-                        key: "key_campana",
-                        label: "Campaña",
-                        width: 320,
-                        render: (val, row) => row.key_campana || "Sin campaña"
-                    },
-                    {
-                        key: "key_proyecto",
-                        label: "Proyecto",
-                        width: 320,
-                        render: (val, row) => row.key_proyecto || "Sin proyecto"
-                    }
-                ]}
-                data={this.state.data}
-            />
-        );
+    applyMapping = () => {
+        const { excelData = [], mapeo = {} } = this.state;
+        const data = excelData.map((row, index) => {
+            const newRow = { key: `row_${index}`, index: index + 1 };
+            for (let col in mapeo) {
+                const mappedKey = mapeo[col];
+                if (mappedKey && mappedKey !== "none") {
+                    newRow[mappedKey] = row[col] || "";
+                }
+            }
+            return newRow;
+        });
+        // }).filter(e => e.telefono && e.telefono.toString().length >= 8);
+
+        this.setState({ data });
     };
 
     render() {
         return (
             <SPage title="Importar contactos desde Excel" disableScroll>
-                {this.renderTable()}
-                {this.renderImportButton()}
+                {this.state.data ? (
+                    <STable
+                        header={[
+                            { key: "index", label: "#", width: 40 },
+                            { key: "cliente", label: "Nombre completo", width: 250 },
+                            { key: "telefono", label: "Teléfono", width: 250 },
+                            {
+                                key: "key_campana",
+                                label: "key_campana",
+                                width: 320,
+                                render: () => this.key_campana || "Sin key_campana"
+                            },
+                            {
+                                key: "key_proyecto",
+                                label: "key_proyecto",
+                                width: 320,
+                                render: () => this.key_proyecto || "Sin proyecto"
+                            }
+                        ]}
+                        data={this.state.data}
+                    />
+                ) : (
+                    <SView center flex>
+                        <SText color={STheme.color.lightGray}>📂 Aún no se ha importado ningún archivo</SText>
+                    </SView>
+                )}
+
+                <SView
+                    backgroundColor='white'
+                    style={{
+                        position: "absolute",
+                        top: 20,
+                        right: "30%",
+                        borderRadius: 4,
+                        overflow: "hidden",
+                    }}
+                    width={140}
+                    height={50}
+                    center
+                    onPress={this.handleExcelImport}
+                >
+                    <SView row>
+                        <SIcon name='Reload' width={15} />
+                        <SView width={10} />
+                        <SText color='black' fontSize={18}>Importar</SText>
+                    </SView>
+                </SView>
             </SPage>
         );
     }
