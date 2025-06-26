631009391e5b5a85ee2be09f2fea0cda3026286e
rickypazd <35882906+rickypazd@users.noreply.github.com>
2025-06-26 05:03:21 -0400

    asd

--- src/MDL/index.ts
+++ src/MDL/index.ts
@@ -2,9 +2,11 @@ import empresa from "./empresa";
 import factura from "./factura";
 import crm from "./crm";
 import whatsapp from "./whatsapp";
+import usuario from "./usuario"
 export default {
   empresa: new empresa(),
   factura: new factura(),
   crm: new crm(),
   whatsapp: new whatsapp(),
+  usuario: new usuario()
 };
