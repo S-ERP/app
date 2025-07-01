c11082a516d15f4e42e9913b95617948d95f3edb
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2025-07-01 05:52:32 -0400

    Update turno.js

--- src/Pages/turno.js
+++ src/Pages/turno.js
@@ -68,17 +68,31 @@ export default class Turno extends Component {
 
                 >
                     <DinamicTable.Col key="index" label="#" width={40} data={(e) => e.index + 1} />
+                    <DinamicTable.Col key={"foto"} label='User'
+                        data={(e) => e.row?.key_usuario}
+                        width={35}
+                        customComponent={e => <SView style={{
+                            width: 24,
+                            height: 24,
+                            borderRadius: 100,
+                            overflow: "hidden",
+                            backgroundColor: STheme.color.card + "66",
+                        }}>
+                            <SImage src={SSocket.api.root + "usuario/" + e.data} style={{
+                                resizeMode: "cover",
+                            }} />
+                        </SView>} />
                     <DinamicTable.Col key="nombre_dia" label="Día" width={100} data={(e) => e.row?.nombre_dia} />
                     <DinamicTable.Col key="horario" label="Horario" width={150} data={(e) => e.row?.horario} />
                     <DinamicTable.Col key="nombre_turno" label="Turno" width={150} data={(e) => e.row?.nombre_turno} />
                     <DinamicTable.Col key="atiende_feriado" label="¿Feriado?" width={100} data={(e) => e.row?.atiende_feriado} />
                     <DinamicTable.Col key="dia_semana" label="Día #" width={80} data={(e) => e.row?.dia_semana} />
                     <DinamicTable.Col key="registrado_el" label="Fecha Registro" width={120} data={(e) => e.row?.registrado_el} />
-                    <DinamicTable.Col key="key_usuario" label="Usuario" width={250} data={(e) => e.row?.key_usuario} />
+                    {/* <DinamicTable.Col key="key_usuario" label="Usuario" width={250} data={(e) => e.row?.key_usuario} /> */}
                     <DinamicTable.Col key="asdsad" label="Usuario" width={250} data={(e) => e.row?.usuario.Nombres} />
 
 
-             </DinamicTable>
+                </DinamicTable>
             </SPage>
         );
     }
