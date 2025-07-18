6299a38523a6a43d867998a6926a0449c5d26688
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2025-07-18 04:14:28 -0400

    update ya dio en windows

--- webpack.dev.js
+++ webpack.dev.js
@@ -25,7 +25,7 @@ module.exports = merge(common, {
         rules: [
             {
                 test: /\.(js|jsx|tsx|ts)$/,
-                exclude: /node_modules\/(?!expo-gl)/,
+                exclude: /node_modules/,
                 use: [{
                     loader: 'babel-loader',
                     options: {
