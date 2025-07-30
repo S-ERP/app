8f37de0d31c64ad4a49a359976fa36426c7192c3
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2025-07-30 03:26:05 -0400

    Update Main.js

--- src/Pages/punto_venta/Main.js
+++ src/Pages/punto_venta/Main.js
@@ -7,6 +7,9 @@ import Categoria from "./Components/Categoria";
 import SIconApp from "../../Assets/SIconApp";
 
 export default class Main extends Component {
+
+
+
     constructor(props) {
         super(props);
         this.selectedTipoKey = "all";
@@ -25,73 +28,110 @@ export default class Main extends Component {
     };
 
     renderCarrito() {
-        return <Carrito ref={(ref) => (this.carritoRef = ref)} onModificarStock={(key, delta) => this.modeloRef?.modificarStock(key, delta)} />;
-    }
-
-    renderProductos() {
-        if (this.tamañoMovil) return;
-        return (
-            <>
-                <SView col="xs-12 sm-12 md-7.5 lg-9" style={{ borderRightWidth: 1, borderRightColor: STheme.color.card }}>
-                    <Categoria
-                        onSelect={this.setTipoKey}
-                        selected={this.selectedTipoKey}
-                        value={this.searchText}
-                        onChangeText={this.setSearchText}
-                    />
-                    <Modelo
-                        ref={(ref) => (this.modeloRef = ref)}
-                        tipoKey={this.selectedTipoKey}
-                        searchText={this.searchText}
-                        onPressProducto={(producto) => this.carritoRef?.addProducto(producto)}
-                    />
-                </SView>
-
-                <SView col="xs-0 sm-12 md-4.5 lg-3" style={{ padding: 16, borderLeftWidth: 1, borderLeftColor: STheme.color.card }}> {this.renderCarrito()} </SView>
-            </>
-        );
-    }
-
-
-
-    renderMovil() {
-        if (!this.tamañoMovil) return;
-
-        return (
-            <>
-                <SView col="xs-12 sm-0 md-0" style={{ padding: 16, borderLeftWidth: 1, borderLeftColor: STheme.color.card }}> {this.renderCarrito()} </SView>
-
-                <SView col="xs-0 sm-12 md-12"
-                    style={{ padding: 16, borderLeftWidth: 1, borderLeftColor: STheme.color.card }}
-                    onPress={() => { this.tamañoMovil = false; this.forceUpdate(); }}
-                >
-                    <SText>Recargar Página: {JSON.stringify(this.tamañoMovil)}</SText>
-                </SView>
-            </>
-        );
+        if (!this._carritoInstance) {
+            this._carritoInstance = (
+                <Carrito
+                    ref={(ref) => (this.carritoRef = ref)}
+                    onModificarStock={(key, delta) => this.modeloRef?.modificarStock(key, delta)}
+                />
+            );
+        }
+        return this._carritoInstance;
     }
 
     btnFlotante() {
-        return <SView col="xs-12 md-0 ">
-            <SView backgroundColor="#3B82F6" border={STheme.color.text} style={{ position: "absolute", bottom: 20, right: 20, width: 56, height: 56, borderRadius: 28, justifyContent: "center", alignItems: "center" }}
-                onPress={() => {
-                    this.tamañoMovil = !this.tamañoMovil;
-                    this.forceUpdate();
-                }}
-            >
-                <SIconApp name="carritoproducto" width={28} height={28} fill={STheme.color.text}   />
+        return (
+            <SView col="xs-12 md-0 ">
+                <SView
+                    backgroundColor="#3B82F6"
+                    border={STheme.color.text}
+                    style={{
+                        position: "absolute",
+                        bottom: 20,
+                        right: 20,
+                        width: 56,
+                        height: 56,
+                        borderRadius: 28,
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                    onPress={() => {
+                        this.tamañoMovil = !this.tamañoMovil;
+                        this.forceUpdate();
+                    }}
+                >
+                    <SIconApp name="carritoproducto" width={28} height={28} fill={STheme.color.text} />
+                </SView>
             </SView>
-        </SView>
+        );
     }
 
     render() {
         return (
             <SPage disableScroll hidden>
                 <Header />
-                <SView col="xs-12" row flex backgroundColor={STheme.color.background} >
-                    {this.renderProductos()}
-                    {this.renderMovil()}
+                <SView col="xs-12" row flex backgroundColor={STheme.color.background}>
+                    {/* 🖥️ Sección de productos */}
+                    <SView
+                        col="xs-12 sm-12 md-7.5 lg-9"
+                        style={{
+                            display: this.tamañoMovil ? "none" : "flex",
+                            borderRightWidth: 1,
+                            borderRightColor: STheme.color.card,
+                        }}
+                    >
+                        <Categoria
+                            onSelect={this.setTipoKey}
+                            selected={this.selectedTipoKey}
+                            value={this.searchText}
+                            onChangeText={this.setSearchText}
+                        />
+                        <Modelo
+                            ref={(ref) => (this.modeloRef = ref)}
+                            tipoKey={this.selectedTipoKey}
+                            searchText={this.searchText}
+                            onPressProducto={(producto) => this.carritoRef?.addProducto(producto)}
+                        />
+
+
+
+                    </SView>
+
+                    {/* 🛒 Sección del carrito */}
+                    <SView
+                        col={this.tamañoMovil ?"xs-12":"xs-12 sm-12 md-4.5 lg-3"}
+                        style={{
+                            display: this.tamañoMovil ? "none" : "flex",
+
+                            padding: 16,
+                            borderLeftWidth: 1,
+                            borderLeftColor: STheme.color.card,
+                        }}
+                    >
+                        {this.renderCarrito()}
+
+                        {/* Solo visible en modo móvil */}
+
+                    </SView>
+
+
+                    {this.tamañoMovil &&
+                            <SView                         col={"xs-12 sm-12 md-4.5 lg-3"}
+
+                                onPress={() => {
+                                    this.tamañoMovil = false;
+                                    this.forceUpdate();
+                                }}
+                                style={{ marginTop: 16 }}
+                            >
+
+                                {/* {this.renderCarrito()} */}
+
+                                <SText>Volver a productos</SText>
+                            </SView>
+                        }
                 </SView>
+
                 {this.btnFlotante()}
             </SPage>
         );
