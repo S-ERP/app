1f620c3d5f94a1a99bd14986115ff58be9fa5587
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2025-08-01 23:29:59 -0400

    update

--- src/Pages/punto_venta/Components/Foto/PopupConfirmaPago.js
+++ src/Pages/punto_venta/Components/Foto/PopupConfirmaPago.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { SView, SText, STheme, SForm, SPopup, SInput, SMath, SNotification } from 'servisofts-component';
 import SIconApp from '../../../../Assets/SIconApp';
 import ResumenTotales from '../Carrito/ResumenTotales';
+import carrito from '../../../../Model/inventario/carrito';
+import Model from '../../../../Model';
 
 export default class PopupConfirmaPago extends Component {
     static open(props) {
@@ -19,10 +21,42 @@ export default class PopupConfirmaPago extends Component {
 
     form = null;
     variableGlobal = "";
+    carritoFormateado = carrito;
+    clienteFormateado = carrito;
+    vendedorFormateado = {};
+    cajaFormateado = {};
+
+
+    dataFormateada({ carrito = [], cliente = null, caja = null, vendedor = null }) {
+        const carritoFormateado = carrito.map(item => ({
+            key: item.key,
+            descripcion: item.descripcion,
+            precio_compra: item.precio_compra ?? 0,
+            precio_venta: item.precio_venta ?? 0,
+            stock: item.stock ?? 0,
+            cantidad: item.cantidad ?? 0,
+            key_marca: item.key_marca ?? null,
+            marca_descripcion: item.marca?.descripcion ?? null,
+            key_tipo_producto: item.key_tipo_producto ?? null,
+            tipo_producto: item.tipo_producto?.descripcion ?? null,
+        }));
+        const clienteFormateado = cliente?.key;
+        const vendedorFormateado = vendedor?.key;
+        return {
+            carrito: carritoFormateado,
+            key_cliente: clienteFormateado ?? null,
+            key_vendedor: vendedorFormateado ?? null,
+            caja: caja,
+        };
+    }
 
     render() {
         const defaultData = this.props?.data?.cliente ?? {};
         const { subtotal, totalImpuesto, totalDescuento, totalFinal, numeroIva, conFactura } = this.props;
+        const { carrito, cliente } = this.props;
+
+
+
 
         return (
             <SView col="xs-12">
@@ -49,103 +83,141 @@ export default class PopupConfirmaPago extends Component {
                             this.forceUpdate();
                         }}
                     />
-                    <SView height={20} />
-                    <SView center row style={{ justifyContent: "space-between", marginBottom: 40 }}>
-                        <SText fontSize={16} color={STheme.color.text}>Cambio:</SText>
-                        <SText fontSize={18} bold color={totalFinal > this.variableGlobal ? "red" : "green"}    >
-                            Bs {this.variableGlobal ? SMath.formatMoney((this.variableGlobal - totalFinal) || 0, 2) : 0}
-                        </SText>
+
+                </SView>
+                <SView height={8} />
+
+                <SView height={20} />
+                <SView center row style={{ justifyContent: "space-between", marginBottom: 40 }}>
+                    <SText fontSize={16} color={STheme.color.text}>Cambio:</SText>
+                    <SText fontSize={18} bold color={totalFinal > this.variableGlobal ? "red" : "green"}    >
+                        Bs {this.variableGlobal ? SMath.formatMoney((this.variableGlobal - totalFinal) || 0, 2) : 0}
+                    </SText>
+                </SView>
+                <SView height={8} />
+                <SView height={8} />
+
+                <SView center row style={{ height: 50 }} >
+                    <SView center flex style={{ borderColor: STheme.color.card, borderWidth: 2, borderRadius: 4, height: 50 }}
+                        onPress={() => {
+
+                            carritoFormateado = [];
+                            clienteFormateado = {};
+                            vendedorFormateado = {};
+                            cajaFormateado = {};
+
+                            this.props.onReload();
+                            this.forceUpdate();
+                            SPopup.close("popup_config_horario");
+
+                        }}
+                    >
+                        <SText color={STheme.color.text}>Cancelar</SText>
                     </SView>
 
+                    <SView width={8} />
 
-                    <SView center row >
-
-                        <SView center flex style={{ borderColor: STheme.color.card, borderWidth: 2, borderRadius: 4, height: 40 }}
-                            onPress={() => {
-                                // this.showPaymentModal = false;
-                                // this._recibido = "";
-                                // this._devolvido = "";
-                                // this.data.cliente = "";
-                                // this.props.onReload();
-                                // this.forceUpdate();
-                                SPopup.close("popup_config_horario");
-
-                            }}
-                        >
-                            <SText color={STheme.color.text}>Cancelar</SText>
-                        </SView>
-                        <SView width={8} />
+                    <SView center flex style={{ backgroundColor: "#18181b", borderColor: STheme.color.gray, borderWidth: 1, borderRadius: 4, height: 50 }}
+                        onPress={() => {
 
 
-                        <SView center flex style={{ backgroundColor: "#18181b", borderColor: STheme.color.gray, borderWidth: 2, borderRadius: 4, height: 40 }}
-                            onPress={() => {
-
-
-                                if (!this.variableGlobal || parseFloat(this.variableGlobal) < totalFinal) {
-                                    SNotification.send({
-                                        title: "Error",
-                                        body: `Monto insuficiente para pagar`,
-                                        type: "error",
-                                        color: STheme.color.error,
-                                        time: 5000,
-                                    });
-                                    return;
-                                }
-                                // const carritoFormateado = this.carrito.map(item => ({
-                                //     key: item.key,
-                                //     descripcion: item.descripcion,
-                                //     precio_compra: item.precio_compra ?? 0,
-                                //     precio_venta: item.precio_venta ?? 0,
-                                //     stock: item.stock ?? 0,
-                                //     cantidad: item.cantidad ?? 0,
-                                //     key_marca: item.key_marca ?? null,
-                                //     marca_descripcion: item.marca?.descripcion ?? null,
-                                //     key_tipo_producto: item.key_tipo_producto ?? null,
-                                //     tipo_producto: item.tipo_producto?.descripcion ?? null,
-                                // }));
-                                // const datos = this.dataFormateada({
-                                //     caja: {
-                                //         subtotal: SMath.formatMoney(subtotal, 2),
-                                //         IVA: SMath.formatMoney(totalImpuesto, 2),
-                                //         Descuento: SMath.formatMoney(totalDescuento, 2),
-                                //         totalFinal: SMath.formatMoney(totalFinal, 2),
-                                //         montoRecibido: SMath.formatMoney(this._recibido, 2),
-                                //         cambio: SMath.formatMoney((this._recibido - totalFinal), 2),
-                                //         conFactura: conFactura ? "si" : "no",
-                                //     },
-
-                                //     carrito: this.props.carrito,
-                                //     cliente: this.data.cliente,
-                                //     // cliente: this.props.data ? this.props.data?.cliente : this.data.cliente,
-                                //     // cliente: this.data?.cliente,
-                                //     vendedor: Model.usuario.Action.getUsuarioLog()
+                            if (!this.variableGlobal || parseFloat(this.variableGlobal) < totalFinal) {
+                                SNotification.send({
+                                    title: "Error",
+                                    body: `Monto insuficiente para pagar`,
+                                    type: "error",
+                                    color: STheme.color.error,
+                                    time: 5000,
+                                });
+                                return;
                             }
 
 
-                                // console.log("🧾 Venta Formateada:");
-                                // console.log(JSON.stringify(datos, null, 2));
+                            const carritoFormateado = carrito(item => ({
+                                key: item?.key ?? null,
+                                descripcion: item?.descripcion ?? null,
+                                precio_compra: item?.precio_compra ?? 0,
+                                precio_venta: item?.precio_venta ?? 0,
+                                stock: item?.stock ?? 0,
+                                cantidad: item?.cantidad ?? 0,
+                                key_marca: item?.key_marca ?? null,
+                                marca_descripcion: item.marca?.descripcion ?? null,
+                                key_tipo_producto: item?.key_tipo_producto ?? null,
+                                tipo_producto: item?.tipo_producto?.descripcion ?? null,
+                            }));
+
+                            clienteFormateado = cliente
+                            vendedorFormateado = Model.usuario.Action.getUsuarioLog()
+
+                            cajaFormateado = {
+                                subtotal: SMath.formatMoney(subtotal, 2),
+                                IVA: SMath.formatMoney(totalImpuesto, 2),
+                                Descuento: SMath.formatMoney(totalDescuento, 2),
+                                totalFinal: SMath.formatMoney(totalFinal, 2),
+                                montoRecibido: SMath.formatMoney(this._recibido, 2),
+                                cambio: SMath.formatMoney((this._recibido - totalFinal), 2),
+                                conFactura: conFactura ? "si" : "no",
+                            };
 
 
-                                // this.showPaymentModal = false;
-                                // this._recibido = "";
-                                // this._devolvido = "";
-                                // this.data.cliente = "";
-                                // this.props.onReload();
-                                // this.forceUpdate();
-                                // SPopup.close("PopupPago");
+
+                            const datos = this.dataFormateada(carritoFormateado, clienteFormateado, vendedorFormateado, cajaFormateado);
+
+                            // const carritoFormateado = this.carrito.map(item => ({
+                            //     key: item.key,
+                            //     descripcion: item.descripcion,
+                            //     precio_compra: item.precio_compra ?? 0,
+                            //     precio_venta: item.precio_venta ?? 0,
+                            //     stock: item.stock ?? 0,
+                            //     cantidad: item.cantidad ?? 0,
+                            //     key_marca: item.key_marca ?? null,
+                            //     marca_descripcion: item.marca?.descripcion ?? null,
+                            //     key_tipo_producto: item.key_tipo_producto ?? null,
+                            //     tipo_producto: item.tipo_producto?.descripcion ?? null,
+                            // }));
+                            // const datos = this.dataFormateada({
+                            //     caja: {
+                            //         subtotal: SMath.formatMoney(subtotal, 2),
+                            //         IVA: SMath.formatMoney(totalImpuesto, 2),
+                            //         Descuento: SMath.formatMoney(totalDescuento, 2),
+                            //         totalFinal: SMath.formatMoney(totalFinal, 2),
+                            //         montoRecibido: SMath.formatMoney(this._recibido, 2),
+                            //         cambio: SMath.formatMoney((this._recibido - totalFinal), 2),
+                            //         conFactura: conFactura ? "si" : "no",
+                            //     },
+
+                            //     carrito: this.props.carrito,
+                            //     cliente: this.data.cliente,
+                            //     // cliente: this.props.data ? this.props.data?.cliente : this.data.cliente,
+                            //     // cliente: this.data?.cliente,
+                            //     vendedor: Model.usuario.Action.getUsuarioLog()
+
+
+                            console.log("🧾 Venta Formateada:");
+                            console.log(JSON.stringify(datos, null, 2));
 
 
 
-                            }
-                        >
-                            <SText color={STheme.color.background}>Confirmar Pago</SText>
-                        </SView>
+                            carritoFormateado = [];
+                            clienteFormateado = {};
+                            vendedorFormateado = {};
+                            cajaFormateado = {};
+
+
+                            this.props.onReload();
+                            this.forceUpdate();
+                            SPopup.close("popup_config_horario");
+                        }
 
 
 
+                        }
+                    >
+                        <SText color={STheme.color.background}>Confirmar Pago</SText>
                     </SView>
+                </SView>
 
-                    {/*
+                {/*
                 <SForm
                     row
                     ref={(ref) => this.form = ref}
@@ -176,9 +248,6 @@ export default class PopupConfirmaPago extends Component {
                         }
                     }}
                 /> */}
-                </SView>
-                <SView height={16} />
-                <SText fontSize={16} center color={totalFinal > this.variableGlobal ? "red" : "green"}   >Valor ingresado: {this.variableGlobal}</SText>
             </SView>
         );
     }
