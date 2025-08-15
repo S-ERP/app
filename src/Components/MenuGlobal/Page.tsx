71907ea2c1fcd7626c9198b76d7e9a98a24f4748
rickypazd <35882906+rickypazd@users.noreply.github.com>
2025-08-15 05:52:06 -0400

    Update Page.tsx

--- src/Components/MenuGlobal/Page.tsx
+++ src/Components/MenuGlobal/Page.tsx
@@ -20,7 +20,7 @@ type PageProps = {
 }
 export default class Page extends React.Component<PageProps> {
     state = {
-        open: true,
+        open: false,
         active: false
     }
     componentDidMount(): void {
