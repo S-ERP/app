b2377badf76534a8cfa260d5c0d125b54a614970
Alvaro Siles Estrada <61075383+alvarosiles11@users.noreply.github.com>
2025-08-29 06:17:56 -0400

    Se agrego foto a la tabla en proveedor
    Se agrego foto a la tabla en proveedor

--- src/Pages/compra/tabla.js
+++ src/Pages/compra/tabla.js
@@ -170,17 +170,17 @@ export default class tabla extends Component {
                 {/* <DinamicTable.Col key="key_asiento_contable" label="asiento" width={150} data={(e) => e.row?.key_asiento_contable ?? ""} /> */}
 
 
-                <DinamicTable.Col key="proveedor" label="Proveedor" width={180} data={(e) => e.row?.proveedor?.razon_social ?? ""}
-                // customComponent={e => <>
-                //     {(e.row?.proveedor?.key_usuario) ?
-                //         <SView col={"xs-12"} center row  >
-                //             <SView style={{ width: 24, height: 24, borderRadius: 100, overflow: "hidden", backgroundColor: STheme.color.card + "66" }}>
-                //                 <SImage src={`${SSocket.api.root}usuario/${e.row?.proveedor?.key_usuario}`} style={{ resizeMode: "cover" }} />
-                //             </SView>
-                //             <SView width={5} />
-                //             <SText color={STheme.color.text}> {e.row?.proveedor?.razon_social}  </SText>
-                //         </SView> : null}
-                // </>}
+                <DinamicTable.Col key="proveedor" label="Proveedor" width={100} data={(e) => e.row?.proveedor?.razon_social ?? ""}
+                    customComponent={e => <>
+                        {(e.row?.proveedor?.key) ?
+                            <SView col={"xs-12"} center row  >
+                                <SView style={{ width: 24, height: 24, borderRadius: 100, overflow: "hidden", backgroundColor: STheme.color.card + "66" }}>
+                                    <SImage src={`${SSocket.api.inventario}proveedor/${e.row?.proveedor?.key}`} style={{ resizeMode: "cover" }} />
+                                </SView>
+                                <SView width={5} />
+                                <SText flex numberOfLines={e.colData.wrap ? 0 : 1} style={e.textStyle}>{e.row?.proveedor?.razon_social}</SText>
+                            </SView> : null}
+                    </>}
                 />
                 <DinamicTable.Col key="admin" label="Admin" width={120} data={(e) => e.row?.usuario?.Nombres ?? ""}
                     customComponent={e => <>
