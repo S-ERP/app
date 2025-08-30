882295dbb7c8ec556406e38c0e560d5c3dc5401c
Alvaro Siles Estrada <61075383+alvarosiles11@users.noreply.github.com>
2025-08-30 02:58:29 -0400

    update

--- src/Pages/inventario/almacen/ReporteConteoInventario.js
+++ src/Pages/inventario/almacen/ReporteConteoInventario.js
@@ -11,6 +11,7 @@ import FloatButtom from '../../../Components/FloatButtom';
 import SIconApp from '../../../Assets/SIconApp';
 import MDL from '../../../MDL';
 import Config from '../../../Config';
+import FloatMenu from '../../../Components/FloatMenu';
 
 export default class ReporteConteoInventario extends Component {
     constructor(props) {
@@ -71,18 +72,69 @@ export default class ReporteConteoInventario extends Component {
 
 
 
+    renderCodigo(codigo) {
+        return <SView row center>
+            <SView border={STheme.color.card} style={{ borderRadius: 8, padding: 6, borderWidth: 1 }}>
+                <SText color={STheme.color.text} fontSize={10} bold>{codigo}</SText>
+            </SView>
+        </SView>
+    }
     mostrarTabla() {
         return <DinamicTable
             key="tabla"
-            // center
             ref={ref => this.table = ref}
             language="es"
             selectType="single"
             {...Config.table.applyTheme()}
 
-            // colors={{ background: STheme.color.background, header: STheme.color.card }}
-            // cellStyle={{ borderWidth: 0 }}
-            // textStyle={{ fontSize: 12, color: "white", textAlign: "center" }}
+            onSelect={(e) => {
+                FloatMenu.open({
+                    e: e.evt,
+                    label: "Opciones",
+                    options: [
+                        {
+                            label: "Ver Detalle Inventario",
+                            // icon: <SIconApp name='Eyes' fill="#e4e4e4ff" width={20} />,
+                            icon: <SIconApp name='Arrow' fill="#e4e4e4ff" width={16} />,
+                            onPress: () => {
+                                SNavigation.navigate("/inventario/almacen/profile/registro_inventario", { pk: e.row.key_almacen, key_conteo: e.row.key_conteo })
+                            }
+                        },
+
+                        {
+                            label: "Anular Registro Cardex",
+                             icon: <SIconApp name='Cerrar' fill="#e00b0bff" width={16} />,
+                            onPress: () => {
+                                SPopup.confirm({
+                                    title: "¿Seguro que quieres eliminar el inventario?",
+                                    message: "El inventario Nro." + e.row?.key_conteo + " será eliminado, si alguien es miembro de la nota puede invitarlo nuevamente.",
+                                    onPress: () => {
+                                        MDL.inventario.anular_cardex(e.row?.key_conteo).then((resp: any) => {
+                                            console.log("anular_cardex", resp);
+                                            this.table.loadData();
+                                        })
+                                    }
+                                })
+                            }
+                        },
+                        // {
+                        //     label: "Imprimir tamaño carta",
+                        //     icon: <SIcon name='imprimir' />,
+                        //     onPress: () => {
+                        //         ReciboCarta.imprimir(e?.row?.key)
+                        //     }
+                        // },
+                        // {
+                        //     label: "Imprimir tipo rollo",
+                        //     icon: <SIcon name='imprimir' stroke="#710505ff" fill='blue' />,
+                        //     onPress: () => {
+                        //         ReciboRollo.imprimir(e?.row?.key)
+                        //     }
+                        // },
+                    ]
+                });
+            }}
+
             loadData={this.loadInitialData.bind(this)}
         >
             <DinamicTable.Col key="index" label="#" width={40} data={(e) => e.index + 1} />
@@ -116,7 +168,7 @@ export default class ReporteConteoInventario extends Component {
                 textStyle={{ fontWeight: "bold" }}
                 customComponent={e => <SText center color={(e.row?.total_perdida >= 1) ? STheme.color.text : STheme.color.lightGray}>{e.row?.total_perdida}</SText>}
             />
-            <DinamicTable.Col key="total_perdida_costo" label="T.Pérdidas Costo" center width={110} data={(e) => e.row?.total_perdida_costo || "0"}
+            <DinamicTable.Col key="total_perdida_costo" label="Costo Pérdidas" center width={100} data={(e) => e.row?.total_perdida_costo || "0"}
                 cellStyle={{ backgroundColor: STheme.color.danger + "33" }}
                 textStyle={{ fontWeight: "bold" }}
                 customComponent={(e) => {
@@ -131,7 +183,7 @@ export default class ReporteConteoInventario extends Component {
             />
 
 
-            <DinamicTable.Col key="total_baja" label="T.Baja" width={60} data={(e) => e.row?.total_baja || "0"}
+            <DinamicTable.Col key="total_baja" label="T.Baja" width={80} data={(e) => e.row?.total_baja || "0"}
                 cellStyle={{ backgroundColor: STheme.color.warning + "33" }}
                 textStyle={{ fontWeight: "bold" }}
                 customComponent={e => <SText center color={(e.row.total_perdida >= 1) ? STheme.color.text : STheme.color.lightGray}>{e.row?.total_baja}</SText>}
@@ -168,11 +220,11 @@ export default class ReporteConteoInventario extends Component {
 
 
 
-            {/* <DinamicTable.Col key="key_asiento" label="Contabilidad" width={150} data={(e) => e.row?.key_conteo}
-                customComponent={e => <SView center style={{ height: 24, borderRadius: 16, overflow: "hidden", backgroundColor: STheme.color.card + "66", borderWidth: 1, borderColor: STheme.color.secondary }}
+            <DinamicTable.Col key="key_asiento" label="Contabilidad" width={124} data={(e) => e.row?.key_conteo}
+                customComponent={e => <SView center style={{ height: 24, borderRadius: 4, overflow: "hidden", backgroundColor: STheme.color.lightGray + "66", borderWidth: 1, borderColor: STheme.color.secondary }}
                     onPress={() => { alert("Generar Asiento contable") }} >
                     <SText >Generar Asiento</SText>
-                </SView>} /> */}
+                </SView>} />
 
 
 
@@ -195,7 +247,16 @@ export default class ReporteConteoInventario extends Component {
             /> */}
 
 
-            <DinamicTable.Col key="key_cardex_anular" label="Inv." width={180} data={(e) => e.row?.key_conteo}
+            <DinamicTable.Col key={"-keyprofile"} label='Ver' width={40} data={(e) => e.row?.key}
+                customComponent={e => <SView row center card padding={2} onPress={() => { SNavigation.navigate("/venta/profile", { pk: e.row.key }) }}>
+                    <SIconApp name='Eyes' height={14} fill={STheme.color.lightGray} ></SIconApp>
+                </SView>} />
+
+            {/* <DinamicTable.Col key={"codigo"} label='Código' width={90} center data={(e) => e?.row?.codigo ?? "AL790"} customComponent={(e) => this.renderCodigo(e.data)} /> */}
+
+
+
+            {/* <DinamicTable.Col key="key_cardex_anular" label="Inv." width={180} data={(e) => e.row?.key_conteo}
                 customComponent={e => {
                     return (e.row.fecha_confirmacion) ?
                         <SView center style={{ height: 24, width: 150, borderRadius: 100, overflow: "hidden", backgroundColor: STheme.color.danger + "66", borderWidth: 1, borderColor: STheme.color.secondary }}
@@ -217,7 +278,7 @@ export default class ReporteConteoInventario extends Component {
                 }
                 }
 
-            />
+            /> */}
 
 
 
@@ -228,25 +289,11 @@ export default class ReporteConteoInventario extends Component {
             <DinamicTable.Col key="hora" label="Hora Creación" width={80} data={(e) => e.row?.hora}
                 customComponent={e => <SView center row><SIconApp name='history' width={12} height={12} fill={STheme.color.lightGray} />
                     <SText color={STheme.color.lightGray}> {e.row?.hora}</SText></SView>} />
-            <DinamicTable.Col key="key_conteo4" label="Ver" width={120} data={(e) => e.row?.key_conteo}
-                customComponent={e => <SView center style={{
-                    padding: 4,
-                    height: 24,
-                    width: 100,
-                    borderRadius: 16,
-                    overflow: "hidden",
-                    backgroundColor: STheme.color.card + "66",
-                    borderWidth: 1,
-                    borderColor: STheme.color.secondary,
-                }}
-                    onPress={() => {
-                        SNavigation.navigate("/inventario/almacen/profile/registro_inventario", { pk: e.row.key_almacen, key_conteo: e.row.key_conteo })
-                    }}
-                >
-                    <SText >Det.Inventario</SText>
-                </SView>} />
 
-            <DinamicTable.Col key={"fecha_confirmacion"} label="F. confirmacion" width={120} dataType="date" data={e => new SDate(e.row?.fecha_confirmacion, "yyyy-MM-ddThh:mm:ss").date} textStyle={{ fontSize: 12, color: STheme.color.text }} dateFormat="yyyy-MM-dd hh:mm" />
+
+        
+
+            <DinamicTable.Col key={"fecha_confirmacion"} label="F. Confirmacion" width={120} dataType="date" data={e => new SDate(e.row?.fecha_confirmacion, "yyyy-MM-ddThh:mm:ss").date} textStyle={{ fontSize: 12, color: STheme.color.text }} dateFormat="yyyy-MM-dd hh:mm" />
             <DinamicTable.Col key="admin" label="Admin" width={120} data={(e) => e.row?.usuario?.Nombres ?? ""}
                 customComponent={e => <>
                     {(e.row?.key_usuario) ?
@@ -257,7 +304,7 @@ export default class ReporteConteoInventario extends Component {
                                 </SView>
                             </SView>
                             <SView width={5} />
-                            <SText center color={STheme.color.text}>{e.row?.usuario?.Nombres}</SText>
+                            <SText center color={STheme.color.lightGray}>{e.row?.usuario?.Nombres}</SText>
                         </SView> : null}
                 </>}
             />
