45eb0b6cb3e2890ffd46b18b742b436ae3f1c37a
Alvaro Siles Estrada <61075383+alvarosiles11@users.noreply.github.com>
2025-09-02 00:23:12 -0400

    update

--- src/Pages/productos/modelo/Components/PopupCrearProveedor.tsx
+++ src/Pages/productos/modelo/Components/PopupCrearProveedor.tsx
@@ -5,8 +5,9 @@ import SSocket from 'servisofts-socket';
 // import MDL from '../../../MDL';
 // import InputFoto from '../../../Components/InputFoto';
 import MDL from '../../../../MDL';
-import { Btn } from '../../../../Components';
+// import { Btn } from '../../../../Components';
 import InputFoto from '../../../../Components/InputFoto';
+import Btn from './Btn';
 // import Btn from './Btn';
 type Props = {
     key_empresa: string,
@@ -51,7 +52,7 @@ export default class PopupCrearProveedor extends Component<Props> {
                 <SForm ref={(ref: any) => this.form = ref} row style={{ justifyContent: "space-between" }}
                     inputs={{
                         "razon_social": {
-                            label: "Razón Social *", placeholder: "Ingresa la razón social", isRequired: true, autoFocus: true, defaultValue: proveedor?.razon_social,
+                            label: "Razón Social *", placeholder: "Ingresa la razón social", disabled: true, isRequired: true, autoFocus: true, defaultValue: proveedor?.razon_social,
                             onSubmitEditing: () => {
                                 if (this.form) this.form.submit();
                             },
@@ -62,79 +63,90 @@ export default class PopupCrearProveedor extends Component<Props> {
                                     style={{ width: 50, height: 50, }} />
                             </SView>,
                         },
-                        "nit": { label: "NIT", placeholder: "NIT", disabled: tre  defaultValue: proveedor?.nit, col: "xs-12" },
-                        "nombre": { label: "Nombre del contacto", placeholder: "Nombre del contacto", defaultValue: proveedor?.nombre, col: "xs-12" },
-                        "telefono": { label: "Teléfono", placeholder: "Teléfono", type: "telefono", defaultValue: proveedor?.telefono, col: "xs-12" },
+                        "nit": { label: "NIT", placeholder: "NIT", disabled: true, defaultValue: proveedor?.nit, col: "xs-12" },
+                        "nombre": { label: "Nombre del contacto", disabled: true, placeholder: "Nombre del contacto", defaultValue: proveedor?.nombre, col: "xs-12" },
+                        "telefono": { label: "Teléfono", placeholder: "Teléfono", disabled: true, type: "telefono", defaultValue: proveedor?.telefono, col: "xs-12" },
 
 
-                        "producto_descripcion": { label: "Producto", placeholder: "Producto", defaultValue: producto_descripcion || "0", col: "xs-12" },
-                        "precio_compra": { label: "Precio", defaultValue: precio_compra || "0", col: "xs-12" },
+                        "producto_descripcion": {
+                            label: "Producto", placeholder: "Producto", defaultValue: producto_descripcion || "0",
+                            style: { paddingStart: 0, fontSize: 10 },
+                            labelStyle: { top: -10, },
+                            inputStyle: { paddingStart: 8, fontSize: 10, backgroundColor: STheme.color.success + "55" },
+                            col: "xs-12"
+                        },
+                        "precio_compra": {
+                            label: "Precio", defaultValue: precio_compra || "0", col: "xs-12",
+
+                            style: { paddingStart: 0, fontSize: 10 },
+                            labelStyle: { top: -10, },
+                            inputStyle: { paddingStart: 8, fontSize: 10, backgroundColor: STheme.color.success + "55" },
+                        }
                     }}
-                // onSubmit={(val: any) => {
-                //     const data = {
-                //         ...val,
-                //         // key_cuenta_contable: "1.0.1",
-                //     };
-                //     if (proveedor?.key) {
-                //         data.key = proveedor?.key;
-                //         MDL.inventario.proveedor.editar(data).then((resp: any) => {
-                //             if (this.props.onSuccess) this.props.onSuccess(resp)
-                //             console.log("eitationnnn " + JSON.stringify(resp))
-                //             if (this._ref.image_sucursal) {
-                //                 const value = this._ref.image_sucursal.getValue();
-                //                 if (Array.isArray(value)) {
-                //                     Upload.sendPromise({ file: value[0], compress: false }, (SSocket.api as any).inventario + "upload/proveedor/" + resp.key)
-                //                 }
-                //             }
-                //             SNotification.send({
-                //                 title: "proveedor guardada",
-                //                 body: "proveedor se ha guardado correctamente.",
-                //                 time: 3000,
-                //                 color: STheme.color.success,
-                //             });
-                //         }).catch((e: any) => {
-                //             if (this.props.onSuccess) this.props.onSuccess(e)
-                //             console.error("Error al guardar el proveedor:", e);
-                //             SNotification.send({
-                //                 title: "Error",
-                //                 body: "No se pudo guardar el proveedor.",
-                //                 time: 3000,
-                //                 color: STheme.color.danger,
-                //             });
-                //         })
-                //     } else {
-                //         MDL.inventario.proveedor.registrar(data).then((resp: any) => {
-                //             if (this.props.onSuccess) this.props.onSuccess(resp)
-                //             if (this._ref.image_sucursal) {
-                //                 const value = this._ref.image_sucursal.getValue();
-                //                 if (Array.isArray(value)) {
-                //                     Upload.sendPromise({ file: value[0], compress: false }, (SSocket.api as any).inventario + "upload/proveedor/" + resp.key)
-                //                 }
-                //             }
-                //             SNotification.send({
-                //                 title: "proveedor guardada",
-                //                 body: "proveedor se ha guardado correctamente.",
-                //                 time: 3000,
-                //                 color: STheme.color.success,
-                //             });
-                //             console.log("Actualizae error")
-                //         }).catch((e: any) => {
-                //             if (this.props.onSuccess) this.props.onSuccess(e)
-                //             console.error("Error al guardar la proveedor:", e);
-                //             SNotification.send({
-                //                 title: "Error",
-                //                 body: "No se pudo guardar la proveedor.",
-                //                 time: 3000,
-                //                 color: STheme.color.danger,
-                //             });
-                //         })
-                //     }
-                // }}
+                onSubmit={(data: any) => {
+                    
+
+                    alert("Pronto funcionada..... se editara el precio al actualiuzar "+JSON.stringify(data))
+                    // if (proveedor?.key) {
+                    //     data.key = proveedor?.key;
+                    //     MDL.inventario.proveedor.editar(data).then((resp: any) => {
+                    //         if (this.props.onSuccess) this.props.onSuccess(resp)
+                    //         console.log("eitationnnn " + JSON.stringify(resp))
+                    //         if (this._ref.image_sucursal) {
+                    //             const value = this._ref.image_sucursal.getValue();
+                    //             if (Array.isArray(value)) {
+                    //                 Upload.sendPromise({ file: value[0], compress: false }, (SSocket.api as any).inventario + "upload/proveedor/" + resp.key)
+                    //             }
+                    //         }
+                    //         SNotification.send({
+                    //             title: "proveedor guardada",
+                    //             body: "proveedor se ha guardado correctamente.",
+                    //             time: 3000,
+                    //             color: STheme.color.success,
+                    //         });
+                    //     }).catch((e: any) => {
+                    //         if (this.props.onSuccess) this.props.onSuccess(e)
+                    //         console.error("Error al guardar el proveedor:", e);
+                    //         SNotification.send({
+                    //             title: "Error",
+                    //             body: "No se pudo guardar el proveedor.",
+                    //             time: 3000,
+                    //             color: STheme.color.danger,
+                    //         });
+                    //     })
+                    // } else {
+                    //     MDL.inventario.proveedor.registrar(data).then((resp: any) => {
+                    //         if (this.props.onSuccess) this.props.onSuccess(resp)
+                    //         if (this._ref.image_sucursal) {
+                    //             const value = this._ref.image_sucursal.getValue();
+                    //             if (Array.isArray(value)) {
+                    //                 Upload.sendPromise({ file: value[0], compress: false }, (SSocket.api as any).inventario + "upload/proveedor/" + resp.key)
+                    //             }
+                    //         }
+                    //         SNotification.send({
+                    //             title: "proveedor guardada",
+                    //             body: "proveedor se ha guardado correctamente.",
+                    //             time: 3000,
+                    //             color: STheme.color.success,
+                    //         });
+                    //         console.log("Actualizae error")
+                    //     }).catch((e: any) => {
+                    //         if (this.props.onSuccess) this.props.onSuccess(e)
+                    //         console.error("Error al guardar la proveedor:", e);
+                    //         SNotification.send({
+                    //             title: "Error",
+                    //             body: "No se pudo guardar la proveedor.",
+                    //             time: 3000,
+                    //             color: STheme.color.danger,
+                    //         });
+                    //     })
+                    // }
+                }}
                 />
             </ScrollView>
             <SHr h={16} />
             <SView row col={"xs-12"}>
-                {/* {this.props.onCancel && <>
+                {this.props.onCancel && <>
                     <Btn type='danger' label='CANCELAR' onPress={() => {
                         if (this.props.onCancel) this.props.onCancel()
                     }} />
@@ -142,7 +154,9 @@ export default class PopupCrearProveedor extends Component<Props> {
                 </>}
                 <Btn type='primary' label='GUARDAR' onPress={() => {
                     if (this.form) this.form.submit();
-                }} /> */}
+                }} />
+
+
             </SView>
         </SView>
     }
