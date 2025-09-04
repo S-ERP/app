7fef56bb9a7b4656a5f2dc4ee41061598b8a4ee8
Alvaro Siles Estrada <61075383+alvarosiles11@users.noreply.github.com>
2025-09-04 03:13:22 -0400

    Update index.ts

--- src/Pages/proveedor/index.ts
+++ src/Pages/proveedor/index.ts
@@ -1,12 +1,14 @@
 import { SPage } from "servisofts-component";
  import Lista from "./Lista";
 import Perfil from "./Perfil";
+import Pagos from "./Pagos";
 export const Parent = {
   name: "proveedor",
   path: `/proveedor`,
 };
 export default SPage.combinePages(Parent.name, {
   "": Lista,
+  "pagos": Pagos,
   lista: Lista,
   perfil: Perfil,
 });
