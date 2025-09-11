8ec167b0b3addf6fbea2507c9bbedb5831725fd1
Licethd <liceth.dfe@gmail.com>
2025-09-11 03:34:14 -0400

    AVANCE: 11/09: By Liceth: Hacer el front para que un usuario elija sus íconos en el sistema dentro de Empresa #21

---
+++ src/Pages/empresa/config/configIcon.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { SHr, SIcon, SImage, SLoad, SNavigation, SPage, SPopup, SText, STheme, SView } from "servisofts-component";
+import MDL from "../../../MDL";
+import SSocket from "servisofts-socket";
+import FloatButtom from "../../../Components/FloatButtom";
+import SIconApp from "../../../Assets/SIconApp";
+import PopupCrearSucursal from "./Components/PopupCrearSucursal";
+import Pizarra from "../../../Components/Pizarra/Pizarra";
+import PizarraNodo from "../../../Components/Pizarra/PizarraNodo";
+import Puerto from "../../../Components/Pizarra/Puerto";
+import Recargar from "../../../Components/Recargar";
+import PopupCrearPuntoVenta from "./Components/PopupCrearPuntoVenta";
+import PopupCrearAlmacen from "../../inventario/almacen/Components/PopupCrearAlmacen";
+import SIconEmpresa, { buildIconEmpresa, inconParams } from "../../../Assets/SIconEmpresa";
+
+export default class configIcon extends React.Component {
+
+    componentDidMount() {
+        this.loadData();
+        MDL.erp.addServerListener({
+            key: "config_2_almacen_editar",
+            component: "almacen",
+            type: "editar",
+            key_empresa: MDL.empresa.select?.key,
+            callback: (data) => {
+                this.loadData();
+            }
+        })
+    }
+    componentWillUnmount() {
+        MDL.erp.removeServerListener({
+            key: "config_2_almacen",
+        })
+    }
+    loadData = async () => {
+        MDL.empresa._full = null;
+        this.empresa = await MDL.empresa.getFull();
+        this.almancenes = await MDL.inventario.getAllAlmacen();
+        this.empresa.sucursales.map(e => {
+            e.almacenes = this.almancenes.filter(almacen => almacen.key_sucursal == e.key);
+        })
+        this.forceUpdate();
+    }
+    render() {
+        console.log("render", inconParams);
+        const empresa = this.empresa;
+        if (!empresa) return <SLoad />
+        const space = 300;
+        return <SPage title={"Configurar íconos"} center>
+            <SView col={"xs-11.5 sm-10 md-8"} >
+                <SHr height={40} />
+                <SText bold fontSize={14} >Elije los íconos de tu preferencia:</SText>
+                <SHr height={20} />
+                <SView col={"xs-12"}>
+                    {/* <SIconEmpresa type='editar' /> */}
+                    {Object.entries(inconParams).map(([categoria, items]) => (
+                        <SView col={"xs-12"} row >
+                            <SView col={"xs-9"} row card padding={10} style={{ marginBottom: 8 }}>
+                                <SText fontSize={15} bold capitalize>{categoria}</SText>
+                                <SHr height={8} />
+                                {items.map(item => (
+                                    <SView width={70} center>
+                                        <SView
+                                            onPress={() => {
+
+                                            }}
+                                            row
+                                            width={40}
+                                            height={40}
+                                            center
+                                            backgroundColor={STheme.color.background}
+                                            style={{ borderRadius: 8 }}
+                                        >
+                                            {/* <SIconEmpresa type={item} />  */}
+                                            {/* <SIconApp name={buildIconEmpresa(item)} width={24} height={24} /> */}
+                                            <SIcon name={item} width={24} height={24} fill={STheme.color.text} />
+
+                                        </SView>
+                                        <SHr height={4} />
+                                        <SText center>{item}</SText>
+                                    </SView>
+                                ))}
+                            </SView>
+                            <SView col={"xs-0.5"} />
+                            <SView col={"xs-2.5"} row card padding={10} style={{ marginBottom: 8 }}>
+                                <SText fontSize={14} bold capitalize justify>Por defecto:</SText>
+                                <SHr height={8} />
+                                <SView col={"xs-12"} center>
+                                    <SView
+                                        onPress={() => {
+
+                                        }}
+                                        row
+                                        width={40}
+                                        height={40}
+                                        center
+                                        backgroundColor={STheme.color.background}
+                                        style={{ borderRadius: 8 }}
+                                    >
+                                        <SIconEmpresa  type={categoria} />
+                                    </SView>
+                                </SView>
+                            </SView>
+                        </SView>
+                    ))}
+                </SView>
+
+            </SView>
+        </SPage >
+    }
+}
