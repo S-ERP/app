a9a1ace1d1eb7c3e37447e40202297ec249b5019
juan3-1416 <jpmonsa1907@gmail.com>
2025-09-26 19:23:44 -0400

    .

--- src/Pages/productos/tipo_producto/table.js
+++ src/Pages/productos/tipo_producto/table.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { SNavigation, SPage, SPopup, SText, STheme, SView } from 'servisofts-component';
+import { SImage, SNavigation, SPage, SPopup, SText, STheme, SView } from 'servisofts-component';
 import { DinamicTable } from 'servisofts-table';
 import Config from '../../../Config';
 import MDL from '../../../MDL';
@@ -8,8 +8,15 @@ import FloatMenu from '../../../Components/FloatMenu';
 import SIconApp from '../../../Assets/SIconApp';
 import FormularioTipoProducto from '../Components/FormularioTipoProducto';
 import FloatButtom from '../../../Components/FloatButtom';
+import SSocket from 'servisofts-socket';
 
 export default class table extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: new Date().getTime()
+        };
+    }
 
 
     async loadData() {
@@ -97,6 +104,10 @@ export default class table extends Component {
                         </View>
                     }} />
                 <DinamicTable.Col key={"descripcion"} wrap label="Descripcion" data={e => e.row.descripcion} width={300}
+                    customComponent={e => <ImageLabel {...e}
+                        src={SSocket.api.inventario + "tipo_producto/.128_" + e.row.key + "?date=" + this.state.time}
+                        srcPreview={SSocket.api.inventario + "tipo_producto/" + e.row.key + "?date=" + this.state.time}
+                    />}
                     textStyle={{
                         fontSize: 14,
                     }} />
@@ -145,4 +156,25 @@ export default class table extends Component {
             }} />
         </SPage>
     }
+}
+const ImageLabel = (props) => {
+    return <SView row style={{
+        alignItems: "center",
+    }}>
+        <SView style={{
+            width: 30,
+            height: 30,
+            borderRadius: 4,
+            overflow: "hidden",
+            backgroundColor: STheme.color.card + "66",
+        }}>
+            <SImage src={props.src} enablePreview
+                srcPreview={props.srcPreview}
+                style={{
+                    resizeMode: "cover",
+                }} />
+        </SView>
+        <SView width={8} />
+        <SText flex style={props.textStyle} numberOfLines={1} >{props.data}</SText>
+    </SView>
 }
