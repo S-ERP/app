6cd942afa7d532a2b09afb75d56ea088e76098f7
Alvaro Siles Estrada <61075383+alvarosiles11@users.noreply.github.com>
2025-10-30 07:38:21 -0400

    Update test3.js

--- src/Pages/test3.js
+++ src/Pages/test3.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import { View, Text, FlatList } from 'react-native';
 import { SDate, SHr, SInput, SNavigation, SPage, SPopup, SText, STheme, SView } from 'servisofts-component';
 import FloatButtom from '../Components/FloatButtom';
+import { FlatList } from 'react-native';
 // import DiaItem from './DiaItem';
 // import TurnoComponent from '.';
 // import MDL from '../../MDL';
@@ -20,39 +21,24 @@ export default class test3 extends Component {
       key: "tag-popup",
       content: (
         <SView col={"xs-11 sm-10 md-8"} backgroundColor={STheme.color.background} style={{ borderRadius: 8, maxWidth: 550 }} padding={16} withoutFeedback>
-          <SText fontSize={18} bold>Aplicar etiquetas a este producto</SText>
-          <SHr height={16} />
 
+          <SView backgroundColor="#0329ffff" height={48} center>
+            <SText fontSize={18} bold>Aplicar etiquetas a este modelo</SText>
+          </SView>
+          <SHr height={8} />
 
+          <SInput placeholder="Filtrar etiquetas" value={this.search} onChangeText={(text) => { this.search = text; this.openTagPopup(); }} />
+          <SHr height={8} />
 
-          <SInput
-            placeholder="Filtrar etiquetas"
-            value={this.search}
-            onChangeText={(text) => {
-              this.search = text;
-              this.openTagPopup(); // reabre popup con búsqueda actualizada
-            }}
-            style={{ marginBottom: 12 }}
-          />
+          <SView style={{ maxHeight: 300 }}>
+            <FlatList
+              data={this.allTags.filter(tag => tag.toLowerCase().includes(this.search.toLowerCase()))}
+              keyExtractor={(item) => item}
+              ItemSeparatorComponent={() => <SHr height={6} />}
 
-          <SView style={{ maxHeight: 300 }}  >
-            {this.allTags
-              .filter(tag => tag.toLowerCase().includes(this.search.toLowerCase()))
-              .map(tag => (
-
-                // un cheack box
-                // y boton de color
+              renderItem={({ item: tag }) => (
                 <SView
-                  key={tag}
-                  row
-                  // center
-                  height={44}
-                  style={{
-                    backgroundColor: this.selectedTags.includes(tag) ? '#1d4ed8' : '#ff7300ff',
-                    borderRadius: 4,
-                    marginBottom: 6,
-                    // paddingHorizontal: 12
-                  }}
+                  col={"xs-12"} key={tag} card row center height={44} style={{ backgroundColor: this.selectedTags.includes(tag) ? 'transparent' : '#fd9946ff', }}
                   onPress={() => {
                     if (!this.selectedTags.includes(tag)) this.selectedTags.push(tag);
                     else this.selectedTags = this.selectedTags.filter(t => t !== tag);
@@ -60,60 +46,53 @@ export default class test3 extends Component {
                     this.forceUpdate();
                   }}
                 >
-                  {/* <SView col={"xs-12"} row backgroundColor='green' center> */}
+                  <SView width={32} backgroundColor={STheme.color.pink} center row border={"green"}>
+                    <SInput center type="checkBox" value={this.selectedTags.includes(tag)}
+                      onChangeText={() => {
+                        if (!this.selectedTags.includes(tag)) this.selectedTags.push(tag);
+                        else this.selectedTags = this.selectedTags.filter(t => t !== tag);
+                        this.openTagPopup();
+                        this.forceUpdate();
+                      }}
+                    /> </SView>
+
+                  <SView width={32}  center row border={"green"}>
+
+                  <SView center width={14} height={14} style={{ backgroundColor: "green", borderRadius:24 }}/>
+                   </SView>
 
 
-                  <SInput type="checkBox" center value={this.selectedTags.includes(tag)}
-                    onChangeText={() => {
-                      if (!this.selectedTags.includes(tag)) this.selectedTags.push(tag);
-                      else this.selectedTags = this.selectedTags.filter(t => t !== tag);
+
+                  <SView flex backgroundColor={STheme.color.pink} row border={"pink"}>
+                    <SText center color={this.selectedTags.includes(tag) ? '#fff' : '#111'} fontSize={14} bold={this.selectedTags.includes(tag)}> {tag} </SText>
+                    {/* {this.selectedTags.includes(tag) && <SText style={{ marginLeft: 'auto', color: '#93c5fd' }}>✓</SText>} */}
+                  </SView>
+
+                </SView>
+              )}
+              ListFooterComponent={() => (
+                this.search && !this.allTags.some(t => t.toLowerCase() === this.search.toLowerCase()) ? (
+                  <SView center height={44} backgroundColor="#ecfdf5" style={{ borderRadius: 12, marginTop: 8 }}
+                    onPress={() => {
+                      const newTag = this.search.trim();
+                      if (newTag && !this.allTags.includes(newTag)) {
+                        this.allTags.push(newTag);
+                        this.selectedTags.push(newTag);
+                      }
+                      this.search = "";
                       this.forceUpdate();
                     }}
-                  />
-
-                  <SText color={this.selectedTags.includes(tag) ? '#fff' : '#111'} fontSize={14} bold={this.selectedTags.includes(tag)}> {tag} </SText>
-                  {this.selectedTags.includes(tag) && (<SText style={{ marginLeft: 'auto', color: '#93c5fd' }}>✓</SText>)}
-                  {/* </SView> */}
-                </SView>
-              ))}
-
-            {/* Crear nueva etiqueta */}
-            {this.search && !this.allTags.some(t => t.toLowerCase() === this.search.toLowerCase()) && (
-              <SView
-                center
-                height={44}
-                backgroundColor="#ecfdf5"
-                style={{ borderRadius: 12, marginTop: 8 }}
-                onPress={() => {
-                  const newTag = this.search.trim();
-                  if (newTag && !this.allTags.includes(newTag)) {
-                    this.allTags.push(newTag);
-                    this.selectedTags.push(newTag);
-                  }
-                  this.search = "";
-                  this.openTagPopup();
-                  this.forceUpdate(); // muestra etiquetas seleccionadas en la página
-
-                }}
-              >
-                <SText color="#059669" fontSize={14}>+ Crear nueva etiqueta "{this.search}"</SText>
-              </SView>
-            )}
+                  >
+                    <SText color="#059669" fontSize={14}>+ Crear nueva etiqueta "{this.search}"</SText> </SView>) : null)}
+            />
           </SView>
 
-          <SHr height={16} />
-          <SView
-            height={48}
-            backgroundColor="#10b981"
-            center
-            style={{ borderRadius: 12 }}
-            onPress={() => {
-              SNavigation.navigate("/tag")
-              SPopup.close('tag-popup');
-            }}
-          >
-            <SText color="#fff" bold>Editar etiquetas</SText>
+          <SHr height={8} />
+
+          <SView backgroundColor="#0329ffff" height={48} center onPress={() => { SNavigation.navigate("/tag"); SPopup.close('tag-popup'); }} >
+            <SText fontSize={16} bold>Editar etiquetas</SText>
           </SView>
+
         </SView>
       )
     });
