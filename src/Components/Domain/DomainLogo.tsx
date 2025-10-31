4ec6f7a9e799f9fd7286cef240bf7dd4b78b0a18
rickypazd <35882906+rickypazd@users.noreply.github.com>
2025-10-31 08:02:35 -0400

    casa

--- src/Components/Domain/DomainLogo.tsx
+++ src/Components/Domain/DomainLogo.tsx
@@ -5,7 +5,7 @@ import { SImage, SText } from "servisofts-component";
 const DomainLogo = (props: SImage["props"]) => {
   const hostname = window.location.hostname;
 
-  let logo = "/capitalcorp/logo512.png"
+  let logo = "/__servisofts/logo512.png"
   if (hostname.includes("capitalcorp-holdings.com")) logo = "/capitalcorp/logo512.png"
   if (hostname.includes("servisofts.com")) logo = "/__servisofts/logo512.png"
 
