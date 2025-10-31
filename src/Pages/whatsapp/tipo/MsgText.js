e73ee9cc562f9d34264adbd776914f005aef2d8c
juan3-1416 <jpmonsa1907@gmail.com>
2025-10-31 12:20:03 -0400

    ..

--- src/Pages/whatsapp/tipo/MsgText.js
+++ src/Pages/whatsapp/tipo/MsgText.js
@@ -1,38 +1,52 @@
 import React, { Component } from "react";
-import { SText, SView, SNavigation } from "servisofts-component";
+import { SText, SView } from "servisofts-component";
 import HoraLabel from "../Comp/HoraLabel";
 import QuotedMsg from "../Comp/QuotedMsg";
 
 export default class MsgText extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
+
+    renderRemitente() {
+        const { mensaje } = this.props;
+
+        // Solo mostrar en chats de grupo y si no es tuyo
+        if (mensaje.fromMe || !mensaje.from || !mensaje.id.remote.includes("@g.us")) return null;
+
+        const nombre = mensaje._data.notifyName || mensaje._data.pushname || mensaje.author || "Desconocido";
+
+        return (
+            <SText
+                color={"#a0d7ff"}
+                fontSize={12}
+                bold
+                style={{
+                    marginBottom: 1,
+                    alignSelf: "flex-start",
+                }}
+            >
+                {nombre}
+            </SText>
+        );
     }
 
-    // Función que separa texto normal y links
     renderTextoConLinks = (texto) => {
         if (!texto) return null;
-
-        // Expresión regular para detectar URLs
         const regex = /(https?:\/\/[^\s]+)/g;
         const partes = texto.split(regex);
 
         return partes.map((parte, i) => {
             if (parte.match(regex)) {
-                // Es un link → clickeable y con estilo
                 return (
                     <SText
                         key={i}
                         color={"#4da6ff"}
                         style={{ textDecorationLine: "underline" }}
-                        onPress={() => SNavigation.openURL(parte)}
+                        onPress={() => window.open(parte, "_blank")}
                     >
                         {parte}
                     </SText>
                 );
             }
 
-            // Detectar texto en negrita con *...*
             const boldParts = parte.split(/(\*.*?\*)/g);
             return boldParts.map((bp, j) => {
                 if (bp.startsWith("*") && bp.endsWith("*")) {
@@ -52,38 +66,41 @@ export default class MsgText extends Component {
     };
 
     render() {
-        const texto = this.props.mensaje.body;
-        const isEnviado = this.props.mensaje.fromMe;
+        const { mensaje, color, key_device } = this.props;
+        const texto = mensaje.body;
+        const isEnviado = mensaje.fromMe;
 
         return (
             <SView
                 style={{
-                    backgroundColor: this.props.color,
+                    backgroundColor: color,
                     borderRadius: 10,
-                    padding: 6,
+                    paddingVertical: 6,
+                    paddingLeft: 8,
+                    paddingRight: 55,
                     maxWidth: "80%",
-                    paddingRight: 80,
-                    alignItems: "flex-start",
+                    alignItems: "flex-end",
+                    alignSelf: isEnviado ? "flex-end" : "flex-start",
                     position: "relative",
                 }}
             >
-                {this.props.mensaje.hasQuotedMsg && (
-                    <QuotedMsg
-                        mensaje={this.props.mensaje}
-                        key_device={this.props.key_device}
-                    />
+                {/* 🔹 Mostrar nombre del remitente si es grupo */}
+                {this.renderRemitente()}
+
+                {mensaje.hasQuotedMsg && (
+                    <QuotedMsg mensaje={mensaje} key_device={key_device} />
                 )}
 
-                <SText fontSize={14} color={"white"}>
+                <SText fontSize={14} color={"white"} style={{ textAlign: "left" }}>
                     {this.renderTextoConLinks(texto)}
                 </SText>
 
                 <HoraLabel
-                    mesaje={this.props.mensaje}
+                    mesaje={mensaje}
                     style={{
                         position: "absolute",
                         bottom: 4,
-                        right: 4,
+                        right: 8,
                     }}
                 />
             </SView>
