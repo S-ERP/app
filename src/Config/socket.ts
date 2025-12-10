bc6365ece30ddd723cb06be345ef0d59044c5434
Alvaro Siles Estrada <61075383+alvarosiles11@users.noreply.github.com>
2025-12-09 22:45:15 -0400

    f5

--- src/Config/socket.ts
+++ src/Config/socket.ts
@@ -6,8 +6,11 @@ export default {
   // ssl: true,
   // host: "serp.servisofts.com",
 
-    ssl: false,
-    host: '192.168.2.1',
+  // ssl: false,
+  // host: '192.168.2.2',
+
+  ssl: false,
+  host: '192.168.2.1',
 
   // ssl: false,
   // host: '192.168.5.48',
