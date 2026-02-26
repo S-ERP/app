342c7d9f8b1dc7b0ced7fa5db4492946e52f9967
Licethd <liceth.dfe@gmail.com>
2026-02-25 23:51:08 -0400

    F5

--- src/Components/MenuGlobal/Barra.tsx
+++ src/Components/MenuGlobal/Barra.tsx
@@ -92,7 +92,7 @@ export default class Barra extends React.Component {
                         {/* </Page> */}
                         {/* <Page label={"Configuracion"} icon={<SIconApp name="Ajustes" fill={STheme.color.text} />} > */}
                         <Page label={"Plan de cuentas"} url={"/conta/cuentas"} permiso="ver" />
-                        <Page label={"Centros de costos"} url={"/conta/centro_costo"} permiso_url="/conta/cuentas" permiso="ver" />
+                        <Page label={"Centros de costos"} url={"/conta/centro_costo2"} permiso_url="/conta/cuentas" permiso="ver" />
                         <Page label={"Diarios"} url={"/conta/diario"} permiso_url="/conta/cuentas" permiso="ver" />
                         {/* <Page label={"Dimensiones"} url={"/conta/dimension"} permiso="ver" /> */}
                         <Page label={"Crear Asiento"} url={"/contabilidad/asiento"} permiso="ver" />
