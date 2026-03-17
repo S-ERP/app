654680ccb410aa8f8cdc1fc28acb1a7aa65845f4
Alvaro Siles Estrada <242724234+alvarosiles@users.noreply.github.com>
2026-03-16 21:36:34 -0400

    Update test.js

--- src/Pages/test.js
+++ src/Pages/test.js
@@ -4,7 +4,8 @@ import * as SPDF from 'servisofts-rn-spdf'
 import MDL from '../MDL';
 import SIconApp from '../Assets/SIconApp';
 
-const fontSize = 12;
+// const fontSize = 12;
+const fontSize = 9;
 const labelSize = 11;
 
 const text = {
@@ -39,199 +40,6 @@ export default class index extends React.Component {
     componentDidMount() {
         this.loadData();
     }
-
-
-    // async loadDatafff() {
-
-    //     const [cajaRaw, usuarios, empresa] = await Promise.all([
-    //         MDL.caja.getByKey(this.key_caja),
-    //         MDL.usuario.getAll(),
-    //         MDL.empresa.getFull()
-    //     ]);
-
-    //     console.clear();
-
-    //     const sucursal = empresa?.sucursales.find(s => s.key === cajaRaw.key_sucursal);
-
-    //     const monedas = empresa?.monedas || [];
-    //     const monedasMap = {};
-
-    //     monedas.forEach(m => {
-    //         monedasMap[m.key] = m;
-    //     });
-
-    //     const caja = {
-    //         ...cajaRaw,
-    //         sucursal,
-    //         cajero: usuarios[cajaRaw.key_usuario],
-    //     };
-
-
-
-    //     const [movimientos, empresa_tipo_pago] = await Promise.all([
-    //         MDL.caja.getDetalle(this.key_caja),
-    //         MDL.caja.empresa_tipo_pago_getAll()
-    //     ]);
-
-
-    //     movimientos.sort(
-    //         (a, b) =>
-    //             new SDate(b.fecha_on, "yyyy-MM-ddThh:mm:ss").getTime() -
-    //             new SDate(a.fecha_on, "yyyy-MM-ddThh:mm:ss").getTime()
-    //     );
-
-
-    //     const tipo_pago = await MDL.caja.tipo_pago_getAll();
-
-    //     const _______aux = await MDL.caja.empresa_tipo_pago_getAll({
-    //         key_punto_venta: caja.key_punto_venta
-    //     });
-
-    //     const data = await MDL.empresa.getFull();
-    //     const cuentas = await MDL.contabilidad.getCuentasCache();
-
-    //     const moneda_base = data.monedas.find(a => a.tipo == "base");
-
-
-
-    //     let pvtp = Object.values(_______aux);
-
-    //     pvtp = pvtp.map(item => {
-
-    //         item.cuenta = cuentas[item.key_cuenta_contable];
-
-    //         const moneda = data.monedas.find(
-    //             a => a.key == item?.cuenta?.key_moneda
-    //         );
-
-    //         item.moneda = moneda ?? moneda_base;
-    //         item.tipo_pago = tipo_pago[item.key_tipo_pago];
-
-    //         item.saldos = 0; aqui tiene que sumar todo lo de movimientosFiltrados
-    //         item.entradas = 0;
-    //         item.salidas = 0;
-    //         return item;
-
-    //     });
-
-
-
-
-    //     const movimientosFiltrados = movimientos.map((m) => {
-
-    //         const etp = empresa_tipo_pago[m.key_empresa_tipo_pago];
-
-    //         console.log(
-    //             "%c" + JSON.stringify(etp, null, 2),
-    //             "color: #2ECC40; font-weight: bold;"
-    //         );
-
-    //         creo que se necesita un filter
-    //         LO vuelvo a guardar pvtp.key =(pvtp.key = etp.key) reduce((sum, mov) => sum + mov.monto, 0);
-    //         LO vuelvo a guardar pvtp.entradas =(pvtp.key = etp.key && mov.monto > 0) reduce((sum, mov) => sum + mov.monto, 0);
-    //         LO vuelvo a guardar pvtp.salidas =(pvtp.key = etp.key && mov.monto < 0) reduce((sum, mov) => sum + mov.monto, 0);
-    //         return {
-    //             hora: new SDate(m.fecha_on).toString("hh:mm"),
-    //             descripcion: m.descripcion,
-    //             persona: usuarios[m.key_usuario]?.Nombres || "",
-    //             tipo: etp?.descripcion || "",
-    //             key_tipo_pago: etp?.key_tipo_pago || "",
-    //             tipo_: m.tipo || "",
-    //             monto: m.monto,
-    //             moneda: monedasMap[m.key_moneda] || null,
-    //         };
-
-    //     });
-
-
-    // pvtp.sort((a, b) => {
-    //         return a.tipo_pago?.orden - b.tipo_pago?.orden;
-    //     });
-
-
-
-    //     console.log(
-    //         "%c" + JSON.stringify(movimientos, null, 2),
-    //         "color: #e100ff; font-weight: bold;"
-    //     );
-
-
-    //     const apertura = Number(cajaRaw.monto_apertura) || 0;
-
-    //     let ventas = {};
-    //     let egresos = 0;
-
-
-
-    //     movimientosFiltrados.forEach((m) => {
-
-    //         if (m.monto > 0) {
-
-    //             if (!ventas[m.tipo]) ventas[m.tipo] = 0;
-
-    //             ventas[m.tipo] += m.monto;
-
-    //         } else {
-
-    //             egresos += m.monto;
-
-    //         }
-
-    //     });
-
-
-
-    //     const resumen = [];
-
-    //     resumen.push({
-    //         label: "Apertura",
-    //         value: apertura
-    //     });
-
-
-    //     Object.keys(ventas).forEach(k => {
-
-    //         resumen.push({
-    //             label: `Ventas ${k}`,
-    //             value: ventas[k]
-    //         });
-
-    //     });
-
-
-    //     if (egresos !== 0) {
-
-    //         resumen.push({
-    //             label: "Traspaso a banca",
-    //             value: egresos
-    //         });
-
-    //     }
-
-
-    //     const total = resumen.reduce(
-    //         (sum, i) => sum + (Number(i.value) || 0),
-    //         0
-    //     );
-
-
-    //     resumen.push({
-    //         label: "Total",
-    //         value: total
-    //     });
-
-
-
-    //     this.setState({
-    //         caja,
-    //         movimientos: movimientosFiltrados,
-    //         resumen,
-    //         ready: true,
-    //         tabla: pvtp,
-    //     });
-
-    // }
-
 
     async loadData() {
 
@@ -567,84 +375,86 @@ export default class index extends React.Component {
 
 
 
-    tabla() {
-        const { tabla } = this.state;
 
-
-        console.clear();
-        console.log("%c" + JSON.stringify(tabla, null, 2), "color: #2ECC40; font-weight: bold;");
-
-
-        return (
-
-            <SPDF.View style={{ width: "100%" }}>
-
-                <SPDF.View style={{ flex: 1 }} />
-
-                {/* <SPDF.View style={{ flex: 2, padding: 10 }}> */}
-
-
-
-                <SPDF.View style={{ width: "100%", height: 50, flexDirection: "row", backgroundColor: "#D0D0D0" }}>
-                    <SPDF.View style={{ flex: 1, borderWidth: 1, justifyContent: "center", padding: 4 }}>
-                        <SPDF.Text style={{ ...text }}>Cuenta </SPDF.Text>
-                    </SPDF.View>
-
-                    <SPDF.View style={{ width: 60, borderWidth: 1, justifyContent: "center", padding: 4 }}>
-                        <SPDF.Text style={{ ...text, fontSize: 8, fontWeight: "bold" }}>Moneda </SPDF.Text>
-                    </SPDF.View>
-
-                    <SPDF.View style={{ width: 60, borderWidth: 1, justifyContent: "center", padding: 4 }}>
-                        <SPDF.Text style={{ ...text, fontSize: 8, fontWeight: "bold" }}>Saldo</SPDF.Text>
-                    </SPDF.View>
-
-                    <SPDF.View style={{ width: 60, borderWidth: 1, justifyContent: "center", padding: 4 }}>
-                        <SPDF.Text style={{ ...text, fontSize: 8, fontWeight: "bold" }}>Entradas</SPDF.Text>
-                    </SPDF.View>
-
-                    <SPDF.View style={{ width: 60, borderWidth: 1, justifyContent: "center", padding: 4 }}>
-                        <SPDF.Text style={{ ...text, fontSize: 8, fontWeight: "bold" }}>Salidas</SPDF.Text>
-                    </SPDF.View>
-                </SPDF.View>
-
-                {tabla.map((item, i) => {
-
-                    return (
-                        <SPDF.View key={i} style={{ width: "100%", height: 28, flexDirection: "row" }}>
-                            <SPDF.View style={{ flex: 1, borderWidth: 1, justifyContent: "center", paddingLeft: 4 }}>
-                                <SPDF.Text style={{ ...text, fontSize: 8 }}> {item.descripcion} {item.tipo_pago.descripcion} {item.moneda.descripcion} </SPDF.Text>
-                            </SPDF.View>
-
-                            <SPDF.View style={{ width: 60, borderWidth: 1, justifyContent: "center", paddingLeft: 4 }}>
-                                <SPDF.Text style={{ ...text, fontSize: 8 }}> {item.moneda.observacion} </SPDF.Text>
-                            </SPDF.View>
-                            <SPDF.View style={{ width: 60, borderWidth: 1, justifyContent: "center", paddingLeft: 4 }}>
-                                <SPDF.Text style={{ ...text, fontSize: 8 }}> {SMath.formatMoney(item.saldos)} </SPDF.Text>
-                            </SPDF.View>
-                            <SPDF.View style={{ width: 60, borderWidth: 1, justifyContent: "center", paddingLeft: 4 }}>
-                                <SPDF.Text style={{ ...text, fontSize: 8 }}> {item.moneda.observacion}  {SMath.formatMoney(item.entradas)} </SPDF.Text>
-                                {/* <SIconApp name='Ingreso' width={8} height={8} /> */}
-                            </SPDF.View>
-                            <SPDF.View style={{ width: 60, borderWidth: 1, justifyContent: "center", paddingLeft: 4 }}>
-                                <SPDF.Text style={{ ...text, fontSize: 8 }}> {item.moneda.observacion}  {SMath.formatMoney(item.salidas)} </SPDF.Text>
-                                {/* <SIconApp name='Egreso' width={8} height={8} /> */}
-                            </SPDF.View>
-                        </SPDF.View>
-                    );
-                })}
-                {/* </SPDF.View> */}
-                // {/* <SPDF.View style={{ flex: 1 }} /> */}
-            </SPDF.View>
-        );
-    }
 
 
     h() {
         return <SPDF.View style={{ width: "100%" }}>
             {this.HeaderCierre()}
             {this.Cajero()}
+            <SPDF.View style={{ width: "100%", alignItems: "center", }}> <SPDF.Text style={text}> DETALLE </SPDF.Text> </SPDF.View>
+            {this.espaciopequeño()}
+            <SPDF.View style={line} />
+            {this.espaciopequeño()}
         </SPDF.View>
     }
+
+
+    tablaDetalle() {
+        const { tabla } = this.state;
+
+
+        console.clear();
+        console.log("%c" + JSON.stringify(tabla, null, 2), "color: #2ECC40; font-weight: bold;");
+        return (
+
+
+
+            <SPDF.View style={{ width: "100%", }}>
+
+                {/* <SPDF.View style={{ width: "100%", alignItems: "center", }}> <SPDF.Text style={text}> Resumen </SPDF.Text> </SPDF.View> */}
+                {this.espaciopequeño()}
+                {/* <SPDF.Text style={{ ...text, width: "100%", fontSize: 8, fontWeight: "bold", alignItems: "center" }}>redsumen</SPDF.Text> */}
+
+                <SPDF.View style={{ width: "80%", height: 44, flexDirection: "row", backgroundColor: "#D0D0D0" }}>
+                    <SPDF.View style={{ flex: 1, borderWidth: 1, height: "100%", justifyContent: "center", padding: 4 }}>
+                        <SPDF.Text style={{ ...text, width: "100%", fontSize: 8, fontWeight: "bold", alignItems: "center" }}>Cuenta</SPDF.Text>
+                    </SPDF.View>
+
+                    <SPDF.View style={{ width: 60, borderWidth: 1, height: "100%", justifyContent: "center", padding: 4 }}>
+                        <SPDF.Text style={{ ...text, width: "100%", fontSize: 8, fontWeight: "bold", alignItems: "center" }}>Moneda</SPDF.Text>
+                    </SPDF.View>
+                    <SPDF.View style={{ width: 60, borderWidth: 1, height: "100%", justifyContent: "center", padding: 4 }}>
+                        <SPDF.Text style={{ ...text, width: "100%", fontSize: 8, fontWeight: "bold", alignItems: "center" }}>Saldo</SPDF.Text>
+                    </SPDF.View>
+                    <SPDF.View style={{ width: 60, borderWidth: 1, height: "100%", justifyContent: "center", padding: 4 }}>
+                        <SPDF.Text style={{ ...text, width: "100%", fontSize: 8, fontWeight: "bold", alignItems: "center" }}>Entradas</SPDF.Text>
+                    </SPDF.View>
+
+                    <SPDF.View style={{ width: 60, borderWidth: 1, height: "100%", justifyContent: "center", padding: 4 }}>
+                        <SPDF.Text style={{ ...text, width: "100%", fontSize: 8, fontWeight: "bold", alignItems: "center" }}>Salidas</SPDF.Text>
+                    </SPDF.View>
+
+                </SPDF.View>
+
+
+                {tabla.map((item, i) => {
+
+                    return (
+                        <SPDF.View key={i} style={{ width: "80%", height: 28, flexDirection: "row" }}>
+                            <SPDF.View style={{ flex: 1, borderWidth: 1, height: "100%", justifyContent: "center", paddingLeft: 4 }}>
+                                <SPDF.Text style={{ ...text, fontSize: 8 }}> {item.descripcion} {item.tipo_pago.descripcion} {item.moneda.descripcion} </SPDF.Text>
+                            </SPDF.View>
+
+                            <SPDF.View style={{ width: 60, borderWidth: 1, height: "100%", justifyContent: "center", paddingLeft: 4 }}>
+                                <SPDF.Text style={{ ...text, fontSize: 8 }}> {item.moneda.observacion} </SPDF.Text>
+                            </SPDF.View>
+                            <SPDF.View style={{ width: 60, borderWidth: 1, height: "100%", justifyContent: "center", paddingLeft: 4 }}>
+                                <SPDF.Text style={{ ...text, fontSize: 8 }}> {SMath.formatMoney(item.saldos)} </SPDF.Text>
+                            </SPDF.View>
+                            <SPDF.View style={{ width: 60, borderWidth: 1, height: "100%", justifyContent: "center", paddingLeft: 4 }}>
+                                <SPDF.Text style={{ ...text, fontSize: 8 }}> {item.moneda.observacion}  {SMath.formatMoney(item.entradas)} </SPDF.Text>
+                            </SPDF.View>
+                            <SPDF.View style={{ width: 60, borderWidth: 1, height: "100%", justifyContent: "center", paddingLeft: 4 }}>
+                                <SPDF.Text style={{ ...text, fontSize: 8 }}> {item.moneda.observacion}  {SMath.formatMoney(item.salidas)} </SPDF.Text>
+                            </SPDF.View>
+                        </SPDF.View>
+                    );
+                })}
+            </SPDF.View>
+        );
+    }
+
     body() {
         return <SPDF.View style={{ width: "100%" }}>
 
@@ -653,10 +463,17 @@ export default class index extends React.Component {
             {this.espacio()}
             {this.Firmas()}
             {this.espacio()}
-            {this.tabla()}
+
+
+            {/* <SPDF.View style={{ width: "100%", height: 10, }}> <SPDF.Text style={text}> Resumen </SPDF.Text> </SPDF.View> */}
+            {/* {this.espaciopequeño()} */}
+
+            {this.tablaDetalle()}
 
         </SPDF.View>
     }
+
+
 
     imprimirPDF() {
 
@@ -665,20 +482,7 @@ export default class index extends React.Component {
         SPDF.create(
 
             <SPDF.Page style={{ width: 612, height: 791, padding: 20 }} header={this.h()} footer={this.pagina()} >
-                {/* {this.espacio()} */}
-
-                {/* <SPDF.Text style={text}> DETALLE </SPDF.Text> */}
-
-                <SPDF.View style={{ width: "100%", alignItems: "center", }}> <SPDF.Text style={text}> DETALLE </SPDF.Text> </SPDF.View>
-
-
                 {this.detalle()}
-
-                {/* {this.espaciopequeño()}
-                <SPDF.View style={line} />
-                {this.espaciopequeño()} */}
-
-
 
                 {this.body()}
 
