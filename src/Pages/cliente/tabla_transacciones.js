0115abede8a9a912d36ac06d4af62a32d515de84
Alvaro Siles Estrada <242724234+alvarosiles@users.noreply.github.com>
2026-04-01 23:06:03 -0400

    Update tabla_transacciones.js

--- src/Pages/cliente/tabla_transacciones.js
+++ src/Pages/cliente/tabla_transacciones.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import { ¡'SPage, SPopup, SView, SText, STheme, SHr, SImage, SDate, SMath } from 'servisofts-component';
 import { SPage, SPopup, SView, SText, STheme, SHr, SImage, SNavigation, SDate, SIcon, SMath, SNotification } from 'servisofts-component';
 import { DinamicTable } from 'servisofts-table';
 import SSocket from 'servisofts-socket';
@@ -14,9 +13,13 @@ export default class TablaTransacciones extends Component {
         super(props);
         this.state = {
             data: {},
-            allArticulos: []
+            fecha_inicio: null,
+            fecha_fin: null,
+            moneda: null,
+            cliente: null,
         };
         this.key = SNavigation.getParam("key");
+
     }
 
     // Componente auxiliar para mostrar fila con imagen + texto
@@ -41,7 +44,7 @@ export default class TablaTransacciones extends Component {
 
             // Traer ventas
             const ventas = await MDL.compra_venta.execute_function("_get_detalles_bycliente", [keyEmpresa, keyCliente]);
-            if (!ventas?.length) throw new Error("No se encontraron detalles para el cliente.");
+            if (!ventas?.length) return [];
 
             // Traer datos relacionados en paralelo
             const [empresa, cliente, usuarios, almacenes] = await Promise.all([
@@ -68,17 +71,26 @@ export default class TablaTransacciones extends Component {
                 cliente: cliente || {},
             }));
 
-            // debo capturar le fecha del primer registro
-            // debo capturar le fecha del ultimo registro
-            // debo capturar le moneda como this.moneda
-            // debo capturar el cliente como this.cliente
+            // 🔥 CAPTURAR DATOS
 
-            this.cliente = cliente || {};
+            this.setState({
+                fecha_fin: ventasEnriquecidas[0]?.fecha_on,
+                fecha_inicio: ventasEnriquecidas[ventasEnriquecidas.length - 1]?.fecha_on,
+                // fecha_inicio: ventasEnriquecidas[0]?.fecha_on,
+                // fecha_fin: ventasEnriquecidas[ventasEnriquecidas.length - 1]?.fecha_on,
+                moneda: ventasEnriquecidas[0]?.moneda,
+                cliente: cliente || {},
+            });
+
 
             console.clear();
+            console.log("%c" + ventasEnriquecidas[0]?.fecha_on, `color: #2ECC40; font-weight: bold;`);
+            console.log("%c" + ventasEnriquecidas[ventasEnriquecidas.length - 1]?.fecha_on, `color: #rgb(231, 35, 1) font-weight: bold;`);
+            console.log("%c" + JSON.stringify(ventasEnriquecidas[0]?.moneda, null, 2), "color: #2ECC40; font-weight: bold;");
             console.log("%c" + JSON.stringify(cliente, null, 2), "color: #2ECC40; font-weight: bold;");
-
             console.log("%cVENTAS ENRIQUECIDAS:", "color: #2ECC40; font-weight: bold;", ventasEnriquecidas);
+
+
             return ventasEnriquecidas;
 
         } catch (error) {
@@ -102,33 +114,10 @@ export default class TablaTransacciones extends Component {
                 keyExtractor={(e) => e.key}
             >
                 <DinamicTable.Col key="index" label="N°" width={30} data={(e) => e.index + 1} />
-
-                {/* <DinamicTable.Col key="sucursal" label="Sucursal" width={180} data={(e) => e.row?.sucursal?.descripcion}
-                    customComponent={e => <this.RowWithImage
-                        keyEntity={e.row?.key_sucursal}
-                        label={e.row?.sucursal?.descripcion}
-                        srcPrefix={`${SSocket.api.empresa}sucursal/`}
-                        styleText={e.textStyle}
-                    />}
-                /> */}
-
-                {/* <DinamicTable.Col key="almacen" label="Almacén" width={140} data={(e) => e.row?.almacen?.descripcion ?? ""}
-                    customComponent={e => <this.RowWithImage
-                        keyEntity={e.row?.almacen?.key}
-                        label={e.row?.almacen?.descripcion}
-                        srcPrefix={`${SSocket.api.empresa}sucursal/`}
-                        styleText={e.textStyle}
-                    />}
-                /> */}
-
-                <DinamicTable.Col key="fecha_on" label="Fecha" width={120} dataType="date"
-                    data={e => new SDate(e.row?.fecha_on, "yyyy-MM-ddThh:mm:ss").date}
-                    textStyle={{ fontSize: 12, color: STheme.color.text }}
-                    dateFormat="yyyy-MM-dd hh:mm"
-                />
+                <DinamicTable.Col key="fecha" label="Fecha" width={140} data={e => new SDate(e.row?.fecha_on).toString("dd/MM/yyyy")} />
                 <DinamicTable.Col key="tipo" label="Tipo" width={100} data={(e) => e.row?.tipo} />
+                <DinamicTable.Col key="detalle" label="Detalle" width={200} data={(e) => e.row?.descripcion} />
 
-                <DinamicTable.Col key="descripcion" label="Descripción" width={150} data={(e) => e.row?.descripcion} />
 
                 <DinamicTable.Col key="precio_unitario_base" label="Precio" width={90} wrap data={(e) => e.row?.precio_unitario_base ?? 0}
                     cellStyle={{ alignItems: "flex-end" }}
@@ -142,69 +131,62 @@ export default class TablaTransacciones extends Component {
                     format={(e) => e.row?.moneda?.observacion + " " + SMath.formatMoney(e.data)}
                 />
 
-                {/* <DinamicTable.Col key="tipo_pago" label="Tipo de Pago" width={120} data={(e) => e.row?.tipo_pago} /> */}
+                <DinamicTable.Col key="debe" label="Debe" width={100}
+                    data={(e) => e.row?.subtotal || 0}
+                    cellStyle={{ alignItems: "flex-end" }}
+                    format={(e) => SMath.formatMoney(e.data)}
+                />
 
-                {/* <DinamicTable.Col key="cliente" label="Cliente" width={120} data={(e) => e.row?.cliente?.nombres ?? ""}
-                    customComponent={e => <this.RowWithImage
-                        keyEntity={e.row?.cliente?.key}
-                        label={e.row?.cliente?.nombres}
-                        srcPrefix={`${SSocket.api.root}usuario/`}
-                        styleText={e.textStyle}
-                    />}
-                /> */}
+                <DinamicTable.Col key="haber" label="Haber" width={100}
+                    data={(e) => 0}
+                    cellStyle={{ alignItems: "flex-end" }}
+                // format={(e) => SMath.formatMoney(e.data)||"d"}
+                />
 
-                {/* <DinamicTable.Col key="moneda" label="Moneda" width={60} wrap data={(e) => e.row?.moneda?.descripcion ?? ""} /> */}
+                <DinamicTable.Col key="saldo" label="Saldo" width={120}
+                    data={(e) => e.row?.subtotal || 0}
+                    cellStyle={{ alignItems: "flex-end" }}
+                    format={(e) => SMath.formatMoney(e.data)}
+                />
 
-                <DinamicTable.Col key="admin" label="Admin" width={120} data={(e) => e.row?.usuario?.Nombres ?? ""}
+                {/* <DinamicTable.Col key="admin" label="Admin" width={120} data={(e) => e.row?.usuario?.Nombres ?? ""}
                     customComponent={e => <this.RowWithImage
                         keyEntity={e.row?.key_usuario}
                         label={e.row?.usuario?.Nombres}
                         srcPrefix={`${SSocket.api.root}usuario/`}
                         styleText={e.textStyle}
                     />}
-                />
+                /> */}
 
             </DinamicTable>
         );
     }
 
     render() {
-
-        const __cliente = this.cliente || {};
-        const __moneda = this.moneda || {
-            "descripcion": "Boliviano",
-            "estado": 1,
-            "tipo": "base",
-            "key_usuario": "b2aa9d81-5f63-40ce-ae35-31fbb1417745",
-            "key_empresa": "f894ea35-5ad1-4b61-a2d0-9294965be169",
-            "fecha_on": "2025-09-05T23:19:05.000589",
-            "tipo_cambio": 1,
-            "key": "2f6b73df-8004-41c1-aa5f-1a81d79d1a8f",
-            "observacion": "BOB"
-        };
-        const __fecha_inicio = "01/Enero/2029";
-        const __fecha_fin = "01/Abril/2029";
-
+        const __cliente = this.state.cliente || {};
+        const __moneda = this.state.moneda || {};
+        const __fecha_inicio = this.state.fecha_inicio ? new SDate(this.state.fecha_inicio).toString("dd-MONTH-yyyy") : "";
+        const __fecha_fin = this.state.fecha_fin ? new SDate(this.state.fecha_fin).toString("dd-MONTH-yyyy") : "";
         return (
             <SPage title="Kardex Individual" disableScroll>
-                <SView row col={"xs-12"} style={{ paddingBottom: 8, paddingLeft: 8, borderBottomWidth: 1, borderColor: STheme.color.lightGray + "30", }}>
-                    <SView width={8} height={"100%"} />
-                    <SView col={"xs-12  "} row center backgroundColor="#e40000"   >
-                        <SView col={"xs-12 sm-5"} backgroundColor="#fff" center   >
-                            {/* Del capturo la fecha de su primer registro y ultimo */}
-                            {/* {this.moneda.descripcion} */}
-                            <SText color='blue' fontSize={15} >Del {__fecha_inicio} al {__fecha_fin}</SText>
-                            <SText color='blue' fontSize={15} >Expresando en {__moneda?.observacion}</SText>
+
+                <SView row col={"xs-12"}>
+                    <SView col={"xs-12  "} row center   >
+                        <SView col={"xs-12 sm-5"} center   >
+                            {/* <SText color={STheme.color.text} fontSize={18} >Kardex Individual</SText> */}
+                            <SHr height={12} />
+                            <SText color={STheme.color.text} fontSize={15} >Del {__fecha_inicio} al {__fecha_fin}</SText>
+                            <SText color={STheme.color.text} fontSize={15} >Expresando en {__moneda?.observacion}</SText>
                         </SView>
                     </SView>
-                    <SView col={"xs-12"} row backgroundColor="#26e400"   >
-                        {/* Del capturo la fecha de su primer registro y ultimo */}
-                        {/* {this.moneda.descripcion} */}
-                        <SText color='blue' fontSize={15} >Cliente :{__cliente?.nombres}</SText>
+
+                    <SHr height={10} />
+
+                    <SView col={"xs-12"} row>
+                        <SText color={STheme.color.text} fontSize={15} >Cliente: {__cliente?.nombres}</SText>
                     </SView>
                 </SView>
-                <SHr height={50} />
-
+                <SHr height={10} />
                 {this.mostrarTabla()}
                 <SHr height={20} />
             </SPage>
