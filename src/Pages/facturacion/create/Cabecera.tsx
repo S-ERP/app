aae888009874a587ddc1f5cd7e9905c88e627736
alvarosiles <242724234+alvarosiles@users.noreply.github.com>
2026-08-13 04:17:11 -0400

    quitando los espacios del nit

--- src/Pages/facturacion/create/Cabecera.tsx
+++ src/Pages/facturacion/create/Cabecera.tsx
@@ -29,7 +29,7 @@ export default class Cabecera extends React.Component<CabeceraProps> {
                     customStyle={customStyle}
                     defaultValue={new SDate(fechaEmision, "yyyy-MM-ddThh:mm:ss").toString("yyyy-MM-dd hh:mm:ss")}
                     onChangeText={e => {
-                        this.props.factura.data.fechaEmision = e
+                        this.props.factura.data.fechaEmision = e.trim()
                     }} />
                 <SHr />
             </SView>
@@ -51,7 +51,7 @@ export default class Cabecera extends React.Component<CabeceraProps> {
                             })}
                             onChangeText={e => {
                                 // console.log(e);
-                                this.props.factura.data.codigoTipoDocumentoIdentidad = e
+                                this.props.factura.data.codigoTipoDocumentoIdentidad = e.trim()
                                 this.setState({ ...this.state })
                             }} />
                     </>}
@@ -85,9 +85,9 @@ export default class Cabecera extends React.Component<CabeceraProps> {
                         })
                     }}
                     onChangeText={e => {
-                        this.props.factura.data.numeroDocumento = e
+                        this.props.factura.data.numeroDocumento = e.trim()
                         if (this.popupBuscarNit) {
-                            this.popupBuscarNit.buscar(e)
+                            this.popupBuscarNit.buscar(e.trim())
                         }
                         // new SThread(1000, "Asda", true).start(() => {
                         //     if (this.props.factura.data.codigoTipoDocumentoIdentidad == "5") {
@@ -111,7 +111,7 @@ export default class Cabecera extends React.Component<CabeceraProps> {
                             // type="select"
                             // options={}
                             onChangeText={e => {
-                                this.props.factura.data.complemento = e
+                                this.props.factura.data.complemento = e.trim()
 
                             }} />
                     </SView>
@@ -126,7 +126,7 @@ export default class Cabecera extends React.Component<CabeceraProps> {
                 <Label bold >{"Nombre/Razón Social"}</Label>
                 <SView width={16} />
                 <SInput ref={ref => this.inprazonsocial = ref ?? undefined} flex customStyle={customStyle} defaultValue={nombreRazonSocial} onChangeText={e => {
-                    this.props.factura.data.nombreRazonSocial = e
+                    this.props.factura.data.nombreRazonSocial = e.trim()
                 }} />
                 <SHr />
                 {/* <Label style={{ width: 90 }} >{"__CLINETE_NIT_"}</Label> */}
@@ -136,7 +136,7 @@ export default class Cabecera extends React.Component<CabeceraProps> {
                 <Label bold >{"Cod. Cliente"}</Label>
                 <SView width={8} />
                 <SInput flex customStyle={customStyle} defaultValue={codigoCliente} onChangeText={e => {
-                    this.props.factura.data.codigoCliente = e
+                    this.props.factura.data.codigoCliente = e.trim()
                 }} />
                 <SHr />
                 {/* <Label style={{ width: 90 }} >{"__CLINETE_NIT_"}</Label> */}
