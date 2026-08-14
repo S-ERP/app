8589166c00abc4515b309cebcd379f0a9259639a
alvarosiles <242724234+alvarosiles@users.noreply.github.com>
2026-08-14 03:46:22 -0400

    update

--- src/Pages/loby/root copy antiguo.js
+++ src/Pages/loby/root copy antiguo.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { ScrollView, Platform } from 'react-native';
-import { SHr, SIcon, SLoad, SNavigation, SPage, SText, STheme, SThread, SView } from 'servisofts-component';
+import { View, Text, ScrollView, Platform } from 'react-native';
+import { SButtom, SHr, SIcon, SLoad, SNavigation, SPage, SText, STheme, SThread, SView } from 'servisofts-component';
 import UsuariosActivos from './Components/UsuariosActivos';
+import Container from '../../Components/Container';
 import MyPerfil from './Components/MyPerfil';
 import Notas from './Components/Notas';
 import MyBilletera from './Components/MyBilletera';
@@ -9,25 +10,38 @@ import Actividades from './Components/Actividades';
 import PerfilEmpresa from './Components/PerfilEmpresa';
 import PHr from '../../Components/PHr';
 import Model from '../../Model';
+import Chat from './Components/Chat';
 import Publicaciones from './Components/Publicaciones';
+import InvitarUsuario from '../../Components/empresa/InvitarUsuario';
 import { ScrollView as ScrollViewGesture } from 'react-native-gesture-handler';
+import SSocket from 'servisofts-socket';
 import MenuOpciones from './Components/MenuOpciones';
 import MenuPaginas from './Components/MenuPaginas';
 
 export default class root extends Component {
-    state = {
-        cargar: true
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            cargar: true
+        };
+        // console.log("loadDataUser", SNavigation.lastRoute)
+    }
+
+    componentDidMount() {
+    }
 
     
 
     renderMunuItem({ onPress, label, icon, color }) {
         return <SView width={(label.length * 8) + 45} card padding={8} onPress={onPress} center row>
+
             <SView width={18} height={18} center>
                 <SIcon name={icon} width={18} height={18} fill={color ?? STheme.color.text} />
             </SView>
             {(label.length > 0) ? <SView width={5} /> : null}
-            <SText>{label}</SText>
+            {/* <SView width={5} /> */}
+            {/* <SView width={8} /> */}
+            <SText >{label}</SText>
         </SView>
     }
     content() {
@@ -49,7 +63,9 @@ export default class root extends Component {
         return <SView col={"xs-12"} center>
             <SView col={"xs-12 sm-10 md-9 lg-7 xl-6 xxl-5"} center >
                 <SHr h={16} />
-                {/* <SButtom type={"outline"}  onPress={() => { SNavigation.navigate("/restaurante") }} >Restaurante</SButtom> */}
+                {/* <SButtom type={"outline"}  onPress={() => {
+                    SNavigation.navigate("/restaurante")
+                 }} >Restaurante</SButtom> */}
                 <MenuOpciones />
                 <PHr />
                 <UsuariosActivos />
@@ -78,29 +94,20 @@ export default class root extends Component {
             web: ScrollView,
             native: ScrollViewGesture
         })
-        return <SPage title="Loby" hidden disableScroll onRefresh={() => {
+        return <SPage title="Loby" hidden disableScroll onRefresh={(e) => {
             this.setState({ cargar: true })
             Model.publicacion.Action.CLEAR();
+            return;
         }} >
-            <Scroll>
+            {/* <SText padding={20} card onPress={() => { SNavigation.navigate("/three") }}>{"IR A THREE"}</SText> */}
+            {/* <SText padding={20} card onPress={() => { SNavigation.navigate("/t2") }}>{"T2"}</SText> */}
+            {/* <SText padding={20} card onPress={() => { SNavigation.navigate("/three/shader") }}>{"Shader Editor"}</SText> */}
+            <Scroll >
                 <SHr h={16} />
                 <PerfilEmpresa />
                 {this.content()}
                 <SHr h={100} />
             </Scroll>
-            <SView style={{
-                position: 'fixed',
-                bottom: 20,
-                right: 20,
-                zIndex: 1000,
-                backgroundColor: "black",
-                borderRadius: 50,
-                padding: 12,
-                boxShadow: '0 4px 12px rgba(0,0,0,0.3)',
-                minWidth: 80,
-            }} center>
-                <SText bold size={13} color="#FFF">v1.0.19</SText>
-            </SView>
         </SPage>
     }
 }
